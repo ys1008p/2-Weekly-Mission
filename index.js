51f7acae6e28c9d20d37c40f26ef4b658ee5e774
BBoBBoM <tkrhkakt12@gmail.com>
2023-11-03 14:56:15 +0900

    mobile tablet media rule css 수정
    태블릿 제목 nav 부분 수정 모바일 화면 javascript로 구성한 부분 제거
    css로만 구현 영역을 하나 더 생성하고 특정 사이즈에서 보여주고 none 으로
    보여주기 처리
    마지막 저장한 링크를 검색 grid 를 이용해서 배치

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-if (window.matchMedia("(max-width: 768px)").matches) {
-const num2ImgDiv = document.getElementById('first-section');
+// document.addEventListener("DOMContentLoaded", function() {
+// if (window.matchMedia("(max-width: 768px)").matches) {
+// const num2ImgDiv = document.getElementById('first-section');
 
-// 원본 요소 복사
-const clonedDiv = num2ImgDiv.cloneNode(true);
 
-// 클래스 이름 추가
-clonedDiv.id ='first-section1'
+// const clonedDiv = num2ImgDiv.cloneNode(true);
 
-// 복사한 요소를 특정 위치에 추가
-const destinationDiv = document.getElementById('first-section-change-location');
-destinationDiv.appendChild(clonedDiv);}
+
+// clonedDiv.id ='first-section1'
+
+
+// const destinationDiv = document.getElementById('first-section-change-location');
+// destinationDiv.appendChild(clonedDiv);}});
