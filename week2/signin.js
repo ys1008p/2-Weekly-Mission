3241a9987ec619b9ef14d687e7af3497826f44db
Parkjiwon <jiwon0324@gmail>
2023-11-10 18:03:11 +0900

    style: signin.css + sigup.css => signin.css

---
+++ week2/signin.js
@@ -0,0 +1,7 @@
+const form = document.qureyselector('#login_form');
+const email = document.qureyselector('#email');
+const password = document.qureyselector('#password');
+const btn = document.qureyselector('button');
+const eye = document.qureyselector('.eye');
+
+
