d613c8fd40bb7cb2302f77573c680007ba844915
So-Yeon Ahn <sozygn@gmail.com>
2023-11-10 14:56:27 +0900

    refactor(utils.js): 재사용 가능한 함수 및 변수 모듈화

--- src/signin.js
+++ src/signin.js
@@ -1,55 +1,13 @@
-const emailInput = document.querySelector('.email-input');
-const pswdInput = document.querySelector('.password-input');
-const pswdEye = document.querySelector('.button-eye');
-const loginBtn = document.querySelector('.button-submit');
-
-const TEST_USER = {
-  email: 'test@codeit.com',
-  password: 'codeit101',
-};
-
-const alertMessageBox = {
-  email: {
-    noInput: '이메일을 입력해주세요.',
-    inappropriateValue: '올바른 이메일 주소가 아닙니다.',
-    loginFail: '이메일을 확인해주세요.',
-  },
-  password: {
-    noInput: '비밀번호를 입력해주세요.',
-    loginFail: '비밀번호를 확인해주세요.',
-  },
-};
-
-// 에러 메세지가 발생해야 하는 위치, 에러 종류를 파라미터로 받고 메세지를 생성
-const makeErrorMessage = function (errorPlace, messageType) {
-  const message = document.createElement('div');
-  message.classList.toggle('alert');
-  message.textContent = alertMessageBox[errorPlace][messageType];
-  switch (errorPlace) {
-    case 'email':
-      emailInput.after(message);
-      break;
-    case 'password':
-      pswdInput.after(message);
-      break;
-  }
-};
-
-// 에러 메세지를 삭제할 위치를 파라미터로 받고 메세지를 삭제
-const eraseErrorMessage = function (errorPlace) {
-  let placeToErase;
-  switch (errorPlace) {
-    case 'email':
-      placeToErase = emailInput;
-      break;
-    case 'password':
-      placeToErase = pswdInput;
-      break;
-  }
-  if (placeToErase.nextElementSibling.className === 'alert') {
-    placeToErase.nextElementSibling.remove();
-  }
-};
+import {
+  alertMessageBox,
+  makeErrorMessage,
+  eraseErrorMessage,
+  TEST_USER,
+  emailInput,
+  pswdInput,
+  pswdEye,
+  loginBtn,
+} from './utils.js';
 
 function errorAlertEmail(e) {
   if (emailInput.value.length === 0) {
