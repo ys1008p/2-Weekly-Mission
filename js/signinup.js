cc6090ec5735fbf27392a8ad0b271e35e858b836
grapefruit13 <liam.heo11@gmail.com>
2023-11-10 23:46:47 +0900

    Feat: 4주차-회원가입 페이지 비밀번호 input 에러케이스 구현

--- js/signinup.js
+++ js/signinup.js
@@ -5,6 +5,9 @@
 // const email_input = document.getElementByClassName("username");
 const email_input = document.querySelector("#username");
 const email_error_text = document.querySelector(".email_error_text");
+const pw_input = document.querySelector("#password");
+const signup_pw_error_text = document.querySelector(".signup_pw_error_text");
+const pw_check_input = document.querySelector("#password_check");
 
 function checkEmail() {
   if (email_input.value === "") {
@@ -28,7 +31,25 @@ function checkEmail() {
   }
 }
 
+function checkPwSignup() {
+  let regex = /^[A-Za-z]+$/;
+
+  if (
+    pw_input.value.length < 8 ||
+    isNaN(pw_input.value) !== true ||
+    regex.test(pw_input.value)
+  ) {
+    pw_input.classList.add("inputError");
+    signup_pw_error_text.innerHTML =
+      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
+    alert(pw_input.value);
+  }
+}
+
 email_input.addEventListener("focusout", checkEmail);
+pw_input.addEventListener("focusout", checkPwSignup);
+
+// 회원가입
 
 // 비밀번호 input에서 focus out 할 때, 값이 8자 미만으로 있거나 문자열만 있거나 숫자만 있는 경우, input에 빨강색 테두리와 아래에 “비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.” 빨강색 에러 메세지가 보입니다.
 // 비밀번호 input과 비밀번호 확인 input의 값이 다른 경우, 비밀번호 확인 input에 빨강색 테두리와 아래에 “비밀번호가 일치하지 않아요.” 빨강색 에러 메세지가 보입니다.
@@ -36,11 +57,11 @@ email_input.addEventListener("focusout", checkEmail);
 // 회원가입을 실행할 경우, 문제가 있는 경우 문제가 있는 input에 빨강색 테두리와 에러 메세지가 보입니다.
 // 이외의 유효한 회원가입 시도의 경우, “/folder”로 이동합니다.
 // 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도경우, “/folder” 페이지로 이동합니다.
+// 회원가입 버튼 클릭 또는 Enter키 입력으로 회원가입 됩니다.
 
+//  로그인
 // 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해주세요.” 에러 메세지가 보입니다.
 // 이외의 로그인 시도의 경우 이메일, 비밀번호 input에 빨강색 테두리와 각각의 input아래에 “이메일을 확인해주세요.”, “비밀번호를 확인해주세요.” 빨강색 에러 메세지가 보입니다.
-
-// 회원가입 버튼 클릭 또는 Enter키 입력으로 회원가입 됩니다.
 
 // 이메일, 비밀번호 input에 에러 관련 디자인을 Components 영역의 에러 케이스로 적용합니다.
 
