221e21daf9e6ab46c618aa82ceac764242b7fec0
Parkjiwon <jiwon0324@gmail>
2023-11-11 22:13:22 +0900

    feat: errAction 함수

--- week2/signin.js
+++ week2/signin.js
@@ -1,7 +1,55 @@
-const form = document.qureyselector('#login_form');
-const email = document.qureyselector('#email');
-const password = document.qureyselector('#password');
-const btn = document.qureyselector('button');
-const eye = document.qureyselector('.eye');
+const form = document.querySelector('#login_form');
+const email = document.querySelector('#email');
+const inputs = document.querySelectorAll('input')
+const password = document.querySelector('#password');
+const checkPassword = document.querySelector('#passWord_check')
+const btn = document.querySelector('button');
+const eye = document.querySelectorAll('.eye');
 
+function errAction (e) {
+  const text = document.createElement('p');
+  text.classList.add('errText')
+  const deleteErrText = document.querySelector('.errText');
+ 
+  if (deleteErrText) {
+    deleteErrText.remove(); 
+    e.target.classList.remove('err');
+    };
 
+  if (e.target === email) {
+    if (email.value === null) {
+      text.textContent = '이메일을 입력해 주세요.';
+    } else if (!isValidEmail(email.value)) {
+      text.textContent = '올바른 이메일 주소가 아닙니다.';
+    } else if (email.value === 'test@codeit.com') {
+      text.textContent = '이미 사용 중인 이메일입니다.';
+    }
+    e.target.classList.add('err');
+    email.after(text);
+
+  } else if (e.target === password) {
+    if (password.value.length < 8 || /^\d+$/.test(password.value) || /^[a-zA-Z]+$/.test(password.value)) {
+      text.textContent = '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.'
+    } else if (password.value === null) {
+      text.textContent = '비밀번호를 입력해 주세요'
+    } 
+    password.after(text);
+    e.target.classList.add('err');
+
+  } else if (e.target === checkPassword) {
+    if (password.value !== checkPassword.value) {
+      text.textContent = '비밀번호가 일치하지 않아요.';
+      checkPassword.after(text);
+      e.target.classList.add('err');
+    }
+  }
+}
+  
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+inputs.forEach(inputElement => {
+  inputElement.addEventListener('focusout', errAction);
+});
