778dc19fb1afe6b22832f765bbf0dd60f39c22d6
youngeun <00eun1128@gmail.com>
2023-11-11 18:00:52 +0900

    recactor(sign) : 파라미터 수정, 코드 위치 수정

--- js/sign.js
+++ js/sign.js
@@ -1,26 +1,22 @@
-const form = document.querySelector('.sign-middle');
 const useremail = document.querySelector('#useremail')
 const signInputEmail = document.querySelector('.sign-input-email');
-const password = document.querySelector('#password');
-const signInputPassword = document.querySelector('.sign-input-password');
 
 // 이메일 타입이 잘못되었는지 확인하는 함수
 function emailTypeChacking (useremail){
   const exptext = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
   if(exptext.test(useremail) === false){
   //이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우			
-  errPaint('올바른 이메일 주소가 아닙니다', signInputEmail, useremail)
+  errPaint('올바른 이메일 주소가 아닙니다', signInputEmail)
   return false;
   }
 }
 
 // 에러문구 표기 함수
-function errPaint(massage, append, signInput){
+function errPaint(massage, append){
   const p = document.createElement('p');
   p.innerText = massage;
   p.classList.add('input-err');
   append.appendChild(p);
-  signInput.value = ""
 }
 
 // 메일 이상 유무 체크
@@ -30,20 +26,24 @@ function chackingEmail () {
     useremail.nextSibling.remove();
   }
   if (!useremail.value){
-    errPaint('이메일을 입력해주세요', signInputEmail, useremail);
+    errPaint('이메일을 입력해주세요', signInputEmail);
   } else if (useremail.value === 'test@codeit.com'){
-    errPaint('이미 사용중인 이메일입니다', signInputEmail, useremail);
+    errPaint('이미 사용중인 이메일입니다', signInputEmail);
   } else{
     emailTypeChacking(useremail.value);
   }   
 }
 
+
 // 비밀번호 타입이 잘못되었는지 확인하는 함수
+const password = document.querySelector('#password');
+const signInputPassword = document.querySelector('.sign-input-password');
+
 function passwordTypeChacking (password){
   const exptext = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
   if(exptext.test(password) === false){
   //이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우			
-  errPaint('비밀번호는 영문,숫자 조합 8자 이상 입력해주세요.', signInputPassword, password)
+  errPaint('비밀번호는 영문,숫자 조합 8자 이상 입력해주세요.', signInputPassword)
   return false;
   }
 }
@@ -55,27 +55,27 @@ function chackingPassword () {
     password.parentNode.childNodes[7].remove();
   }  
   if (!password.value){
-    errPaint('비밀번호를 입력해주세요', signInputPassword, password);
+    errPaint('비밀번호를 입력해주세요', signInputPassword);
   } else{
     passwordTypeChacking(password.value);
   } 
 }
 
-useremail.addEventListener('focusout', chackingEmail);
-password.addEventListener('focusout', chackingPassword);
-
 //비밀번호 재확인
 const password2 = document.querySelector('#password2');
 const signInputPassword2 = document.querySelector('.sign-input-password2');
 
-password2.addEventListener('focusout', doubleCheakingPassword);
-
 function doubleCheakingPassword(){
-  // 에어코드 중복 방지
+  // 에러코드 중복 방지
   if (password2.parentNode.childNodes[7]){
     password2.parentNode.childNodes[7].remove();
   }
   if (password.value !== password2.value){
-    errPaint('비밀번호가 일치하지 않아요', signInputPassword2, password2);
+    errPaint('비밀번호가 일치하지 않아요', signInputPassword2);
   }
 }
+
+useremail.addEventListener('focusout', chackingEmail);
+password.addEventListener('focusout', chackingPassword);
+
+password2.addEventListener('focusout', doubleCheakingPassword);
