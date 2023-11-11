08425c4cf14dd51389d2ac52a2a6b719402418b4
bumsly <qjatmd2005@naver.com>
2023-11-12 07:21:25 +0900

    Generate pw-check validtion function

--- js/signup.js
+++ js/signup.js
@@ -1,56 +1,66 @@
 const email = document.getElementById("email");
 const pw = document.getElementById("pw");
+const pwCheck = document.getElementById("pw-check");
 const emailError = {
   null: document.querySelector(".error-message__null"),
   wrong: document.querySelector(".error-message__wrong"),
   used: document.querySelector(".error-message__used"),
 };
 const pwError = document.querySelector(".error-message__eight");
+const pwCheckError = document.querySelector(".error-message__diff");
 
 // 이메일 유효성 검증
-function checkEmail() {
-  const emailValue = email.value;
-
+function validEmail() {
   // 에러 메시지 초기화
   email.classList.remove("error-input");
   for (let error in emailError) {
     emailError[error].classList.remove("display");
   }
 
-  if (!emailValue.includes("@")) {
+  if (!email.value.includes("@")) {
     email.classList.add("error-input");
-    if (emailValue == "") {
+    if (email.value == "") {
       // 이메일이 비어있다.
       emailError.null.classList.add("display");
     } else {
       // 이메일에 @가 없지만 비어있지 않다.
       emailError.wrong.classList.add("display");
     }
-  } else if (emailValue === "test@codeit.com") {
+  } else if (email.value === "test@codeit.com") {
     // 이미 사용 중인 이메일이다.
     email.classList.add("error-input");
     emailError.used.classList.add("display");
   }
-  // 정상적인 이메일 형태이다.
 }
 
 // 비밀번호 유효성 검증
-function checkPassword() {
-  const pwValue = pw.value;
-
+function validPassword() {
+  // 에러 메시지 초기화
   pw.classList.remove("error-input");
   pwError.classList.remove("display");
 
   if (
-    pwValue.length < 8 || // 값이 8자 미만일 때
-    pwValue.match(/\d+/g) === null || // 문자열만 있을 때
-    pwValue.match(/\D+/g) === null // 숫자만 있을 때
+    pw.value.length < 8 || // 값이 8자 미만이다.
+    pw.value.match(/\d+/g) === null || // 문자열만 있다.
+    pw.value.match(/\D+/g) === null // 숫자만 있다.
   ) {
     pw.classList.add("error-input");
     pwError.classList.add("display");
   }
-  // 정상적인 비밀번호 형태이다.
 }
 
-email.addEventListener("focusout", checkEmail);
-pw.addEventListener("focusout", checkPassword);
+function validPwCheck() {
+  // 에러 메시지 초기화
+  pwCheck.classList.remove("error-input");
+  pwCheckError.classList.remove("display");
+
+  // 비밀번호와 비밀번호 확인의 값이 다르다.
+  if (pw.value !== pwCheck.value) {
+    pwCheck.classList.add("error-input");
+    pwCheckError.classList.add("display");
+  }
+}
+
+email.addEventListener("focusout", validEmail);
+pw.addEventListener("focusout", validPassword);
+pwCheck.addEventListener("focusout", validPwCheck);
