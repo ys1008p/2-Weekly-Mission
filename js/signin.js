2483205d6df7644a7a55aa9889af4f68e2f86506
정현진 <jhj1004v@naver.com>
2023-11-11 17:34:30 +0900

    FEAT : userEmail(input)에 focusout 이벤트 사용시 errorMessage

--- js/signin.js
+++ js/signin.js
@@ -1,13 +1,32 @@
+const userEmail = document.querySelector("#userEmail")
+const errorMessage = document.querySelector(".errorMessage")
 
-const formControlInput = document.querySelectorAll(".formControl > input")
 
-formControlInput.forEach(function(item){
-  item.addEventListener("focusin", (event) => {
-    item.style.outline = "none";
-    item.style.border = "1px solid #6D6AFE";
-  });  
-  item.addEventListener("focusout", (event) => {
-    item.style.border = "1px solid #CCD5E3";
-  });  
+function validateEmailFormat(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
 
-})
+function validateEmail() {
+  const userEmailValue = userEmail.value.trim();
+
+  if (userEmailValue === "") {
+    userEmail.style.border = "1px solid #FF5B56";
+    errorMessage.classList.add("on");
+    errorMessage.innerText = "이메일을 입력해주세요.";
+  } else if (!validateEmailFormat(userEmailValue)) {
+    userEmail.style.border = "1px solid #FF5B56";
+    errorMessage.classList.add("on");
+    errorMessage.innerText = "올바른 이메일 주소가 아닙니다.";
+  } else if (userEmailValue ==="test@codeit.com") {
+    userEmail.style.border = "1px solid #FF5B56";
+    errorMessage.innerText = "이미 사용 중인 이메일입니다.";
+  } 
+  else {
+    userEmail.style.border = "";
+    errorMessage.classList.remove("on");
+    errorMessage.innerText = "";
+  }
+}
+
+userEmail.addEventListener("focusout", validateEmail);
