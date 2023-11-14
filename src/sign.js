e20a135b22d29c4e0eefe09c43730f7c47d70fef
박지선 <shanshan@MacBook-Pro-6.local>
2023-11-14 14:13:23 +0900

    Feat: Add function checkPassword

--- src/sign.js
+++ src/sign.js
@@ -23,12 +23,12 @@ function isValidPassword(password) {
   }
 }
 
-function addErrorMessageEmail() {
-  const inputEmail = document.querySelector("input.email"); //inputEmail: 이메일 입력란
-  const p = document.createElement("p"); //input 박스 밑 에러 메세지 요소 추가
-  p.classList.add("error-message"); //에러 메세지용 p태그에 빨간 글씨 클래스 추가
-  const userEmailInput = inputEmail.value; //사용자의 이메일 입력값
+const inputEmail = document.querySelector("input.email"); //inputEmail: 이메일 입력란
+const emailError = document.createElement("p"); //input 박스 밑 에러 메세지 요소 추가
+emailError.classList.add("error-message"); //에러 메세지용 p태그에 빨간 글씨 클래스 추가
+const userEmailInput = inputEmail.value; //사용자의 이메일 입력값
 
+function addErrorMessageEmail() {
   let message = "";
 
   //상태 체크 후 상황에 맞는 메세지
@@ -44,28 +44,54 @@ function addErrorMessageEmail() {
   }
 
   //input 텍스트 요소에 에러 메세지 추가
-  p.textContent = message;
+  emailError.textContent = message;
 }
 
-input.addEventListener("focusout", () => {
-  //에러
+inputEmail.addEventListener("focusout", () => {
   addErrorMessageEmail();
 });
 
-function addErrorMessagePassword() {
-  const inputPassword = document.querySelector("input.input-password"); //inputPassword: 패스워드 입력란
-  const p = document.createElement("p"); //input 박스 밑 에러 메세지 요소 추가
-  p.classList.add("error-message"); //에러 메세지용 p태그에 빨간 글씨 클래스 추가
-  const userPasswordInput = inputPassword.value; //사용자의 이메일 입력값
+const inputPassword = document.querySelector("input.input-password"); //inputPassword: 패스워드 입력란
+const passwordError = document.createElement("p"); //input 박스 밑 에러 메세지 요소 추가
+passwordError.classList.add("error-message"); //에러 메세지용 p태그에 빨간 글씨 클래스 추가
+const userPasswordInput = inputPassword.value; //사용자의 패스워드 입력값
 
+function addErrorMessagePassword() {
   let message = "";
 
   //상태 체크 후 상황에 맞는 메세지
-  if (!isValidPassword(userPasswordInput)) {
+  if (!userPasswordInput) {
     addRedBorder(inputPassword);
     message = "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
+  } else if (!isValidEmail(userPasswordInput)) {
+    addRedBorder(inputPassword);
+    message = "올바른 이메일 주소가 아닙니다.";
   }
 
   //input 텍스트 요소에 에러 메세지 추가
-  p.textContent = message;
+  passwordError.textContent = message;
 }
+
+inputPassword.addEventListener("focusout", () => {
+  addErrorMessagePassword();
+});
+
+// 비밀번호 확인 입력값이 비밀번호와 일치하는지 확인
+
+const inputPasswordCheck = document.querySelector("input.password-check");
+const passwordCheckError = document.createElement("p"); //input 박스 밑 에러 메세지 요소 추가
+passwordCheckError.classList.add("error-message"); //에러 메세지용 p태그에 빨간 글씨 클래스 추가
+const userPasswordCheckInput = inputPassword.value; //사용자의 패스워드 확인 입력값
+
+function checkPassword() {
+  let message = "";
+
+  if (inputPasswordCheck === inputPassword) {
+    addRedBorder(inputPassword);
+    message = "비밀번호가 일치하지 않아요.";
+  }
+}
+
+inputPasswordCheck.addEventListener("focusout", () => {
+  addErrorMessagePasswordCheck();
+});
