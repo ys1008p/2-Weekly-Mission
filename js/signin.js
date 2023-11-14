a9c513f3ce2e70b725436d8c5ed58e8d860386ba
CheeseB <2489ckckck@naver.com>
2023-11-14 19:11:44 +0900

    feat: 로그인 페이지 기본 요구사항 완료

---
+++ js/signin.js
@@ -0,0 +1,59 @@
+import { removeErrorMessage, setErrorMessage } from './modules/handleError.js';
+import {
+  isAllObjectValueTrue,
+  isEmpty,
+  isValidEmail,
+  verifyEmail,
+  verifyPassword,
+} from './modules/validation.js';
+
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#emailError');
+const password = document.querySelector('#password');
+const passwordError = document.querySelector('#passwordError');
+const signin = document.querySelector('#signin');
+
+const formState = {
+  email: false,
+  password: false,
+};
+
+const checkEmail = (e) => {
+  const value = e.target.value;
+
+  if (isEmpty(value)) {
+    setErrorMessage(email, emailError, '이메일을 입력해주세요.');
+  } else if (!isValidEmail(value)) {
+    setErrorMessage(email, emailError, '올바른 이메일 주소가 아닙니다.');
+  } else {
+    removeErrorMessage(email, emailError);
+    formState.email = verifyEmail(value);
+  }
+};
+
+const checkPassword = (e) => {
+  const value = e.target.value;
+
+  if (isEmpty(value)) {
+    setErrorMessage(password, passwordError, '비밀번호를 입력해주세요');
+  } else {
+    removeErrorMessage(password, passwordError);
+    formState.password = verifyPassword(value);
+  }
+};
+
+const verifyForm = () => {
+  if (isAllObjectValueTrue(formState)) location.href = './folder.html';
+  else {
+    setErrorMessage(email, emailError, '이메일을 확인해주세요.');
+    setErrorMessage(password, passwordError, '비밀번호를 확인해주세요.');
+  }
+};
+
+email.addEventListener('focusout', checkEmail);
+email.addEventListener('focusin', () => removeErrorMessage(email, emailError));
+password.addEventListener('focusout', checkPassword);
+password.addEventListener('focusin', () =>
+  removeErrorMessage(password, passwordError)
+);
+signin.addEventListener('click', verifyForm);
