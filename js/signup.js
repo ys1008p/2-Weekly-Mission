109bf2c3d88447058e1b46b9e5c11bed1cf00c40
grapefruit13 <liam.heo11@gmail.com>
2023-11-15 11:35:15 +0900

    Refactor(mentor): 4주차-js 변수명 카멜케이스로 변경, 이벤트 핸들러함수에 매개변수 e추가하고 e.target 사용, css inputError 클래스명 변경

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,5 @@
 const emailInput = document.querySelector("#username");
-const emailErrorText = document.querySelector(".email_error_text");
+// const emailErrorText = document.querySelector(".email_error_text");
 
 const pwInput = document.querySelector("#password");
 const pwErrorText = document.querySelector(".pw_error_text");
@@ -8,61 +8,77 @@ const pwCheckErrorText = document.querySelector(".pw_check_error_text");
 
 const loginButton = document.querySelector(".login-button");
 
-let validEmail = false;
-let validPw = false;
+// 한 객체에 유효성 검사하는 변수 저장
+const formState = {
+  email: false,
+  pw: false,
+  pwCheck: false,
+};
 
+// error_text div는 미리 만들어두고, e.target의 다음 형제 요소가 선택되도록
 function checkEmail(e) {
+  const emailErrorText = e.target.nextElementSibling;
   if (e.target.value === "") {
-    e.target.classList.add("inputError");
+    e.target.classList.add("error");
+    emailErrorText.classList.add("error");
     emailErrorText.innerHTML = "이메일을 입력해주세요.";
   } else if (e.target.value === "test@codeit.com") {
-    e.target.classList.add("inputError");
+    e.target.classList.add("error");
+    emailErrorText.classList.add("error");
     emailErrorText.innerHTML = "이미 사용 중인 이메일입니다.";
   } else if (e.target.value.includes("@") == false) {
-    e.target.classList.add("inputError");
+    e.target.classList.add("error");
+    emailErrorText.classList.add("error");
     emailErrorText.innerHTML = "올바른 이메일 주소가 아닙니다.";
   } else {
-    e.target.classList.remove("inputError");
+    e.target.classList.remove("error");
+    emailErrorText.classList.remove("error");
     emailErrorText.innerHTML = "";
-    validEmail = true;
+    formState.email = true;
   }
 }
 
 function checkPw(e) {
-  let engOnly = /^[A-Za-z]+$/;
+  const engOnly = /^[A-Za-z]+$/;
+  // pw
+  const pwErrorText = e.target.parentElement.lastElementChild;
 
   if (
     e.target.value.length < 8 ||
-    isNaN(pwInput.value) !== true ||
-    engOnly.test(pwInput.value)
+    isNaN(e.target.value) !== true ||
+    engOnly.test(e.target.value)
   ) {
-    e.target.classList.add("inputError");
+    e.target.classList.add("error");
+    pwErrorText.classList.add("error");
     pwErrorText.innerHTML =
       "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
   } else if (e.target.value === "") {
-    e.target.classList.add("inputError");
+    e.target.classList.add("error");
+    pwErrorText.classList.add("error");
     pwErrorText.innerHTML = "비밀번호를 입력해주세요.";
   } else {
-    e.target.classList.remove("inputError");
+    e.target.classList.remove("error");
+    pwErrorText.classList.remove("error");
     pwErrorText.innerHTML = "";
+    formState.pw = true;
   }
 }
 
-function comparePw(e) {
+function pwCheckConfirmation(e) {
   if (pwInput.value !== e.target.value) {
-    e.target.classList.add("inputError");
-    pwInput.classList.add("inputError");
+    e.target.classList.add("error");
+    pwInput.classList.add("error");
     pwCheckErrorText.innerHTML = "비밀번호가 일치하지 않아요";
   } else {
-    pwInput.classList.remove("inputError");
-    pwChece.targetkInput.classList.remove("inputError");
+    pwInput.classList.remove("error");
+    pwChece.targetkInput.classList.remove("error");
     pwCheckErrorText.innerHTML = "";
-    validPw = true;
+    formState.pwCheck = true;
   }
 }
 
 function signup(e) {
-  if (validEmail && validPw) {
+  if (formState.email && formState.pw) {
     if (e.key === "Enter") {
       location.href = "./folder";
     } else {
@@ -73,6 +89,6 @@ function signup(e) {
 
 emailInput.addEventListener("focusout", checkEmail);
 pwInput.addEventListener("focusout", checkPw);
-pwCheckInput.addEventListener("focusout", comparePw);
+pwCheckInput.addEventListener("focusout", pwCheckConfirmation);
 loginButton.addEventListener("click", signup);
 pwCheckInput.addEventListener("keydown", signup);
