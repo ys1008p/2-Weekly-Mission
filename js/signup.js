7166ee194369c52311d47157685c3d355cb30e0e
grapefruit13 <liam.heo11@gmail.com>
2023-11-16 23:38:27 +0900

    Refactor(mentor): 4주차-주석 삭제

--- js/signup.js
+++ js/signup.js
@@ -9,7 +9,6 @@ const formState = {
   pwConfirmation: false,
 };
 
-// 유효성 검사 후 에러 메시지 띄워야 할 때
 function ifError(e, errorText, errorMsg, input) {
   e.target.classList.add("error");
   errorText.classList.add("error");
@@ -25,7 +24,6 @@ function ifOk(e, errorText, input) {
 }
 
 function checkEmail(e) {
-  // 다음 형제 요소
   const emailErrorText = e.target.nextElementSibling;
   const errorMsg = [
     "이메일을 입력해주세요.",
@@ -35,28 +33,12 @@ function checkEmail(e) {
 
   if (e.target.value === "") {
     ifError(e, emailErrorText, errorMsg[0], "email");
-    // e.target.classList.add("error");
-    // emailErrorText.classList.add("error");
-    // emailErrorText.innerHTML = "이메일을 입력해주세요.";
-    // formState.email = false;
   } else if (e.target.value === "test@codeit.com") {
     ifError(e, emailErrorText, errorMsg[1], "email");
-    // e.target.classList.add("error");
-    // emailErrorText.classList.add("error");
-    // emailErrorText.innerHTML = "이미 사용 중인 이메일입니다.";
-    // formState.email = false;
   } else if (e.target.value.includes("@") == false) {
     ifError(e, emailErrorText, errorMsg[2], "email");
-    // e.target.classList.add("error");
-    // emailErrorText.classList.add("error");
-    // emailErrorText.innerHTML = "올바른 이메일 주소가 아닙니다.";
-    // formState.email = false;
   } else {
     ifOk(e, emailErrorText, "email");
-    // e.target.classList.remove("error");
-    // emailErrorText.classList.remove("error");
-    // emailErrorText.innerHTML = "";
-    // formState.email = true;
   }
 }
 
@@ -70,27 +52,14 @@ function checkPw(e) {
 
   if (e.target.value === "") {
     ifError(e, pwErrorText, errorMsg[0], "pw");
-    // e.target.classList.add("error");
-    // pwErrorText.classList.add("error");
-    // pwErrorText.innerHTML =
-    //   "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
-    // formState.pw = false;
   } else if (
     e.target.value.length < 8 ||
     isNaN(e.target.value) !== true ||
     engOnly.test(e.target.value)
   ) {
     ifError(e, pwErrorText, errorMsg[1], "pw");
-    // e.target.classList.add("error");
-    // pwErrorText.classList.add("error");
-    // pwErrorText.innerHTML = "비밀번호를 입력해주세요.";
-    // formState.pw = false;
   } else {
     ifOk(e, pwErrorText, "pw");
-    // e.target.classList.remove("error");
-    // pwErrorText.classList.remove("error");
-    // pwErrorText.innerHTML = "";
-    // formState.pw = false;
   }
 }
 
@@ -99,18 +68,10 @@ function checkPwConfirmation(e) {
   const errorMsg = "비밀번호가 일치하지 않아요";
   if (pwInput.value !== e.target.value) {
     ifError(e, pwCheckErrorText, errorMsg, "pwConfirmation");
-    // e.target.classList.add("error");
     pwInput.classList.add("error");
-    // pwCheckErrorText.classList.add("error");
-    // pwCheckErrorText.innerHTML = "비밀번호가 일치하지 않아요";
-    // formState.pwConfirmation = false;
   } else {
     ifOk(e, pwCheckErrorText, "pwConfirmation");
-    // e.target.classList.remove("error");
     pwInput.classList.remove("error");
-    // pwCheckErrorText.classList.remove("error");
-    // pwCheckErrorText.innerHTML = "";
-    // formState.pwConfirmation = true;
   }
 }
 
