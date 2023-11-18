f27b8606d81a575418a77d688fe157afdbb3e213
박지원 <jackgg12322@gmail.com>
2023-11-18 11:43:53 +0900

    refactor : 주석제거

--- src/signup.js
+++ src/signup.js
@@ -6,7 +6,7 @@ import {
   userList,
 } from "./import.js";
 
-//이메일
+
 const emailInput = document.querySelector("#email");
 const emailErrorMessage = document.querySelector("#email-error-message");
 emailInput.addEventListener("focusout", (event) =>
@@ -37,7 +37,7 @@ function validateEmailInput(email) {
   removeInputError({ input: emailInput, errorMessage: emailErrorMessage });
 }
 
-//비밀번호
+
 const passwordInput = document.querySelector("#pwd");
 const passwordErrorMessage = document.querySelector("#password-error-message");
 passwordInput.addEventListener("focusout", (event) =>
@@ -64,7 +64,6 @@ function validatePasswordInput(password) {
   });
 }
 
-//비밀번호 확인
 const checkPasswordInput = document.querySelector("#pwd-check");
 const checkPasswordErrorMessage = document.querySelector(
   "#check-error-message"
@@ -90,7 +89,7 @@ function validateCheckPasswordInput(checkPassword) {
   return true;
 }
 
-//회원가입
+
 const signForm = document.querySelector("#form");
 signForm.addEventListener("submit", submitForm);
 function submitForm(event) {
@@ -112,21 +111,20 @@ function submitForm(event) {
   );
 }
 
-//4주차 요구사항
-// 비밀번호 input
+
 const eyeBtn = document.getElementById("eye-btn");
 const pwInput = document.getElementById("pwd");
-// 비밀번호 확인 input
+
 const eyeBtnCheck = document.getElementById("eye-btn-check");
 const pwCheckInput = document.getElementById("pwd-check");
-// eye icon
+
 const eyeOff = document.querySelector(".off-img");
 const eyeOn = document.querySelector(".on-img");
-//eye check
+
 const eyeCheckOn = document.querySelector(".check-on");
 const eyeCheckOff = document.querySelector(".check-off");
 
-//비밀번호 input
+
 eyeBtn.addEventListener("click", () => {
   if (pwInput.type === "password") {
     pwInput.type = "text";
@@ -138,7 +136,7 @@ eyeBtn.addEventListener("click", () => {
     eyeOn.style.display = "none";
   }
 });
-// 비밀번호 확인 input
+
 eyeBtnCheck.addEventListener("click", () => {
   if (pwCheckInput.type === "password") {
     pwCheckInput.type = "text";
