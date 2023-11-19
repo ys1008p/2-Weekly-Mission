10961acb627d584ecef6e1122a00ef8559afb4e9
grapefruit13 <liam.heo11@gmail.com>
2023-11-19 22:31:34 +0900

    Refactor: 5주차-필요없는 코드 및 주석 삭제

--- js/validFunction.js
+++ js/validFunction.js
@@ -1,5 +1,4 @@
 import { formState as signupState } from "./signup.js";
-import { formState as signinState } from "./signin.js";
 
 export const validObject = {
   ifError: function (e, errorText, errorMsg, input) {
@@ -7,13 +6,11 @@ export const validObject = {
     errorText.classList.add("error");
     errorText.innerHTML = `${errorMsg}`;
     signupState[`${input}`]["isValid"] = false;
-    // if (input !== "pwConfirmation") signinState[`${input}`]["isValid"] = false;
   },
   ifOk: function (e, errorText, input) {
     e.target.classList.remove("error");
     errorText.classList.remove("error");
     errorText.innerHTML = "";
     signupState[`${input}`]["isValid"] = true;
-    // if (input !== "pwConfirmation") signinState[`${input}`]["isValid"] = true;
   },
 };
