2a3e209aa8c55a79894ce8dcd757ebfab6b5265a
박지원 <jackgg12322@gmail.com>
2023-11-20 00:54:50 +0900

    feat : 심화 요구사항

--- src/shared.js
+++ src/shared.js
@@ -3,6 +3,11 @@ const ERROR_MESSAGE_CLASSNAME = "error-message-on";
 const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 const PASSWORD_REGEX = /^[a-zA-Z0-9](?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/g;
 
+export const userID = {
+  email: "test@codeit.com",
+  password: "codeit101",
+};
+
 export function setInputError(elements, message) {
   elements.input.className += ` ${SIGN_INPUT_ERROR_CLASSNAME}`;
   elements.errorMessage.className += ` ${ERROR_MESSAGE_CLASSNAME}`;
@@ -26,14 +31,20 @@ export function isPasswordValid(password) {
 export function togglePassword(input, toggleButton) {
   if (input.getAttribute("type") === "password") {
     input.setAttribute("type", "text");
-    toggleButton.getElementsByTagName("img")[0].setAttribute("src", "./images/eye-on.svg");
+    toggleButton
+      .getElementsByTagName("img")[0]
+      .setAttribute("src", "./images/eye-on.svg");
     return;
   }
   input.setAttribute("type", "password");
-  toggleButton.getElementsByTagName("img")[0].setAttribute("src", "./images/eye-off.svg");
+  toggleButton
+    .getElementsByTagName("img")[0]
+    .setAttribute("src", "./images/eye-off.svg");
 }
 
-export const userID = {
-  email: "test@codeit.com",
-  password: "codeit101",
-};
+export function TokenStorage(Storage) {
+  const accessToken = localStorage.getItem("j_1644");
+  if (accessToken) {
+    location.replace(Storage);
+  }
+}
