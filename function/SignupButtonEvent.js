c1bc0080bf4ad65b82fc74e37b617c2c35667db6
BBoBBoM <tkrhkakt12@gmail.com>
2023-11-19 22:06:02 +0900

    미세한 오타 수정과 register.js 파일 삭제

--- function/SignupButtonEvent.js
+++ function/SignupButtonEvent.js
@@ -5,47 +5,49 @@
 3가지 경우에서 모두 true 값이 출력해야 가상으로 회원가입이 되었다고 판단 page를 넘김 
 한개라도 false 값이 있으면 동작 X, false 부분의 오류를 표시해주는 이벤트*/
 import {
-
   passwordInputFocusoutEvent,
   passwordMatchingEvent,
-  emailInputFocusoutEventSignup
+  emailInputFocusoutEventSignup,
 } from "../script/function.js";
-import{  emailInput,passwordInput,}from "../tag.js"
+import { emailInput, passwordInput } from "../tag.js";
+async function signupButton(userEmail, userPassword) {
+  try {
+    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-up", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: userEmail, password: userPassword }),
+    });
 
-async function signupButton(userEmail, userPassword){
-  try{
-    const response = await fetch('https://bootcamp-api.codeit.kr/api/sign-up',{
-      method : 'post',
-      headers : {'Content-Type': 'apllication/json'},
-      body : {email :userEmail,password : userPassword }
-    })
     if (response.ok) {
-      return true
+      return true;
     } else {
-    return false
+      return false;
     }
-  }catch(err){
-    console.log('가입 오류',err)
+  } catch (err) {
+    console.error("가입 오류", err);
+    return false; // 에러 발생 시에도 가입 실패로 처리
   }
 }
 
-
-function buttonClickEvent(e) {
+async function buttonClickEvent(e) {
   e.preventDefault();
   let userConfirmCheck = emailInputFocusoutEventSignup();
   let passwordConfirmCheck = passwordInputFocusoutEvent();
-  let passwordMachingCheck = passwordMatchingEvent();
+  let passwordMatchingCheck = passwordMatchingEvent();
   const userEmail = emailInput.value;
   const userPassword = passwordInput.value;
-  if (
-    userConfirmCheck &&
-    passwordConfirmCheck &&
-    passwordMachingCheck
-  ) {
-    signupButton(userEmail,userPassword)
-    if(signupButton(userEmail,userPassword)==true){    alert(`가입을 축하합니다 ${userEmail}님`);
-    window.location.href = "/folder/folder.html";}
-    
+
+  if (userConfirmCheck && passwordConfirmCheck && passwordMatchingCheck) {
+    const signupResult = await signupButton(userEmail, userPassword);
+
+    if (signupResult) {
+      alert(`가입을 축하합니다 ${userEmail}님`);
+      window.location.href = "/folder/folder.html";
+    } else {
+      // 가입 실패 시의 로직
+      alert("가입에 실패했습니다. 다시 시도해주세요.");
+    }
   }
 }
+
 export { buttonClickEvent };
