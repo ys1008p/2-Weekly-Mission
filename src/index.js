3324d4329c98c87a5795d4b958ffd8132da2f7bc
So-Yeon Ahn <sozygn@gmail.com>
2023-11-23 14:46:10 +0900

    feat: Footer 컴포넌트 제작

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Cards from './components/Cards/Cards';
 import Gnb from './components/Gnb/Gnb';
+import Footer from './components/Footer/Footer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Gnb />);
+root.render(<Footer />);
