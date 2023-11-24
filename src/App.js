40630f29fabaaadb23dae2abd09a513a04932903
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-24 19:52:29 +0900

    feature: folder api 가져오는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import folderData from './folder-mock.json';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import './App.css';
 import SharedMain from './components/SharedMain/SharedMain';
 
 function App() {
-  const { folder } = folderData;
-
   return (
     <div className="App">
       <NavBar />
-      <SharedMain folder={folder} />
+      <SharedMain />
       <Footer />
     </div>
   );
--- src/components/SharedMain/SharedMain.js
+++ src/components/SharedMain/SharedMain.js
@@ -2,15 +2,38 @@ import './SharedMain.css';
 import FolderInfo from '../FolderInfo/FolderInfo';
 import SearchBar from '../atoms/SearchBar/SearchBar';
 import CardList from '../CardList/CardList';
-export default function SharedMain({ folder }) {
-  const { owner, links } = folder;
-  return (
+import { getFolderData } from '../../utils/api';
+import { useEffect, useState } from 'react';
+
+export default function SharedMain() {
+  const [folderData, setFolderData] = useState();
+  const [isLoadingSuccess, setIsLoadingSuccess] = useState(false);
+
+  const handleLoadFolderData = async () => {
+    let result;
+    try {
+      result = await getFolderData();
+    } catch (error) {
+      console.log(error);
+    }
+
+    const { folder } = result;
+    setFolderData(folder);
+    setIsLoadingSuccess(true);
+    console.log('폴더 데이터를 가져왔습니다.');
+  };
+
+  useEffect(() => {
+    handleLoadFolderData();
+  }, []);
+
+  return isLoadingSuccess ? (
     <div>
-      <FolderInfo owner={owner} folderName={folder.name} />
+      <FolderInfo owner={folderData.owner} folderName={folderData.name} />
       <article className="test">
         <SearchBar />
-        <CardList links={links} />
+        <CardList links={folderData.links} />
       </article>
     </div>
-  );
+  ) : null;
 }
