2b50aa643729f7c34cf99003a1338d43d775f471
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-24 19:09:41 +0900

    feature: api에서  폴더 데이터를 가져오는 함수 구현

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,3 +8,12 @@ export async function getUserInfo() {
   const body = await response.json();
   return body;
 }
+
+export async function getFolderData() {
+  const response = await fetch(`${BASE_URL}/api/sample/folder`);
+  if (!response.ok) {
+    throw new Error('폴더 데이터를 불러오는데 실패했습니다.');
+  }
+  const body = await response.json();
+  return body;
+}
