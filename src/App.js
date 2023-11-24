e09248a3cdeb47f2853612a7c1e4899c1aa01ec1
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-24 15:14:03 +0900

    style: 랜딩페이지 메인 내용 스타일

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import FolderInfo from './components/FolderInfo/FolderInfo';
 import SearchBar from './components/atoms/SearchBar/SearchBar';
+import './App.css';
+
 function App() {
   const { folder } = folderData;
   const { owner, links } = folder;
@@ -11,27 +13,28 @@ function App() {
     <div className="App">
       <NavBar />
       <FolderInfo owner={owner} folderName={folder.name} />
-      <div>
+      <article className="test">
         <SearchBar />
+
         <div>
-          <ul>
+          <ul className="CardList">
             {links.map((item) => {
               return (
-                <li key={item.id}>
-                  <div>
+                <li key={item.id} className="Card">
+                  <div className="Card-thumbnail">
                     <img src={item.imageSource} alt="링크 썸네일" />
-                    <div>
-                      <div>{item.createdAt}</div>
-                      <div>{item.description}</div>
-                      <div>{item.createdAt}</div>
-                    </div>
+                  </div>
+                  <div>
+                    <div>{item.createdAt}</div>
+                    <div>{item.description}</div>
+                    <div>{item.createdAt}</div>
                   </div>
                 </li>
               );
             })}
           </ul>
         </div>
-      </div>
+      </article>
       <Footer />
     </div>
   );
