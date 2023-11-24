8175c64882c2af02714d1c821e7fc67bc8954447
So-Yeon Ahn <sozygn@gmail.com>
2023-11-24 19:39:54 +0900

    feat: Profile 컴포넌트 react-responsive 사용해서 모바일 대응

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,17 @@
 import './Profile.css';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Profile({ profile }) {
+  const isMobile = useMediaQuery({
+    query: '(max-width :767px)',
+  });
+  if (isMobile) {
+    return (
+      <div className="profileContainer">
+        <img className="profileImage" src={profile.profileImageSource}></img>
+      </div>
+    );
+  }
   return (
     <div className="profileContainer">
       <img className="profileImage" src={profile.profileImageSource}></img>
