b618be3ea06367e39110f2733bab6318539f33cf
So-Yeon Ahn <sozygn@gmail.com>
2023-11-24 17:36:03 +0900

    feat: FolderBanner 컴포넌트 제작

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import Gnb from './Gnb/Gnb';
 import Footer from './Footer/Footer';
 import { Cards } from './Cards/Cards';
+import FolderBanner from './FolderBanner/FolderBanner';
 function Layout({ items, profile }) {
   return (
     <>
       <Gnb profile={profile}></Gnb>
+      <FolderBanner items={items} />
       <Cards items={items}></Cards>
       <Footer></Footer>
     </>
