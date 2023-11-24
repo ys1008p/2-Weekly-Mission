9ad439b9a310e73b425df8e12efe2bb3ee919f53
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-24 13:56:47 +0900

    style: SearchBar 컴포넌트 개발

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import folderData from './folder-mock.json';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import FolderInfo from './components/FolderInfo/FolderInfo';
+import SearchBar from './components/atoms/SearchBar/SearchBar';
 function App() {
   const { folder } = folderData;
   const { owner, links } = folder;
@@ -11,9 +12,7 @@ function App() {
       <NavBar />
       <FolderInfo owner={owner} folderName={folder.name} />
       <div>
-        <div>
-          <input />
-        </div>
+        <SearchBar />
         <div>
           <ul>
             {links.map((item) => {
---
+++ src/components/atoms/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import './SearchBar.css';
+import SearchIcon from '../../../assets/Search.svg';
+export default function SearchBar() {
+  return (
+    <div className="SearchBar">
+      <input className="SearchBar-input" placeholder="링크를 검색해 보세요." />
+      <img src={SearchIcon} alt="검색 아이콘" className="SearchBar-icon" />
+    </div>
+  );
+}
