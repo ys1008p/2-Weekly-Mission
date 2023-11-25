fed3a3555309cccb88bda69ab623ab018ec858ef
김우현 <rladngus133@naver.com>
2023-11-26 01:30:02 +0900

    feat: user 데이터 없을시 로그인 버튼보이는 기능 추가

--- src/component/shared/header/Navbar.js
+++ src/component/shared/header/Navbar.js
@@ -33,14 +33,21 @@ function Navbar() {
         <a href="/">
           <img src="img/logo.png" alt="logo" className="nav-item-logo" />
         </a>
-        <div className="nav-item-profile">
-          <img
-            src={profileImageSource}
-            alt="profile-img"
-            className="nav-item-profile-img"
-          />
-          <p className="nav-item-profile-email">{email}</p>
-        </div>
+
+        {userData.id[2] ? (
+          <div className="nav-item-profile">
+            <img
+              src={profileImageSource}
+              alt="profile-img"
+              className="nav-item-profile-img"
+            />
+            <p className="nav-item-profile-email">{email}</p>
+          </div>
+        ) : (
+          <div className="nav-item-profile">
+            <button className="nav-login-btn">로그인</button>
+          </div>
+        )}
       </div>
     </nav>
   );
