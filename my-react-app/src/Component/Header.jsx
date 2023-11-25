6fdd2ff8cf7a448c39c9458e2402bed4cac4e204
박지원 <jackgg12322@gmail.com>
2023-11-25 17:45:27 +0900

    feat : Header 영역 구현

--- my-react-app/src/Component/Header.jsx
+++ my-react-app/src/Component/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import ImgHeader from '../images/white.svg'
 import '../CSS/Landing.css'
 
 export default function Header() {
   return (
-    <div>
-      
+    <div className="HeaderContainer">
+    <div className='HeaderBox'>
+      <img src={ImgHeader} alt="코드잇 이미지" className='ImgHeader' />
+      <p className='HeaderText'>@코드잇</p>
+      <p className='HeaderDescription'>⭐️ 즐겨찾기</p>
+      </div>
     </div>
   );
 }
