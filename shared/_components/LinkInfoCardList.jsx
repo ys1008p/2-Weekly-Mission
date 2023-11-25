4c9bf1be8ed86a26c16493290a6c26aa3d399c53
dkile <dkile532@naver.com>
2023-11-26 03:02:35 +0900

    Fix: LinkInfoCard 클릭 시 이동 방법을 새로운 탭으로 수정

--- shared/_components/LinkInfoCardList.jsx
+++ shared/_components/LinkInfoCardList.jsx
@@ -5,7 +5,7 @@ export default function LinkInfoCardList({ linkInfoList }) {
     <ul className="grid-cols-fill-max-34 grid w-full justify-center gap-x-[2rem] gap-y-[2.5rem]">
       {linkInfoList.map((linkInfo) => (
         <li key={linkInfo.id}>
-          <a href={linkInfo.url}>
+          <a href={linkInfo.url} target="_blank">
             <LinkInfoCard linkInfo={linkInfo} />
           </a>
         </li>
