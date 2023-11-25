7babf20318ac23bbb2c82d4db14eba340d275edf
박지원 <jackgg12322@gmail.com>
2023-11-26 04:13:18 +0900

    fix : File명 수정

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './Component/Navbar';
+import NavBar from './Component/NavBar';
 import Header from './Component/Header';
 import Footer from './Component/Footer';
 import Main from './Component/Main';
 import './CSS/Landing.css'
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <>
-    <Navbar/>
+    <NavBar/>
     <Header/>
     <Main/>
     <Footer/>
