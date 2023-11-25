202fe77b38faf8a310d46c3eb46fb45650513836
So-Yeon Ahn <sozygn@gmail.com>
2023-11-25 10:51:15 +0900

    refoctor: mobile 화면에서 유저 이메일 숨기는 조건부 렌더링 구문 가독성 향상

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,17 +5,10 @@ export default function Profile({ profile }) {
   const isMobile = useMediaQuery({
     query: '(max-width :767px)',
   });
-  if (isMobile) {
-    return (
-      <div className="profileContainer">
-        <img className="profileImage" src={profile.profileImageSource}></img>
-      </div>
-    );
-  }
   return (
     <div className="profileContainer">
       <img className="profileImage" src={profile.profileImageSource}></img>
-      <div className="profileEmail">{profile.email}</div>
+      {!isMobile && <div className="profileEmail">{profile.email}</div>}
     </div>
   );
 }
