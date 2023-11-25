eec7014ba5eae41ec0483c0e5a82808273e4d06a
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2023-11-26 00:23:53 +0900

    Footer 컴포넌트 만들기
    - css 스타일
    - 반응형
    - root.css 변수명 변경하여 Main 컴포넌트 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 import '../components/reset.css';
 import '../components/root.css';
 
@@ -8,6 +9,7 @@ function App(){
     <div className="container">
       <Header className="header"/>
       <Main className="main"/>
+      <Footer className="footer"/>
     </div>
   )
 }
