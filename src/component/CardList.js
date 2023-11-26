068ed4292f7f3a6622029ab2f60a14a58cc03233
Jisoo Han <llqrw55@naver.com>
2023-11-26 11:27:29 +0900

    feat: 업로드 시간 바인딩

--- src/component/CardList.js
+++ src/component/CardList.js
@@ -3,16 +3,14 @@ import { useState } from "react";
 import Card from "./Card";
 
 function CardList({links}){
-    const [isHover, setIsHover] = useState(false);
-    const handleHover=()=>{
-        setIsHover(true)
-    }
+    
+
     return(
         <>
             <div className={styles.cardList}>
                 {
                     links && links.map((link)=>(
-                        <Card isHover={isHover} onMouseOver={handleHover} key={link.id} link={link}/>
+                        <Card  key={link.id} link={link}/>
                     ))
                 }
             </div>
