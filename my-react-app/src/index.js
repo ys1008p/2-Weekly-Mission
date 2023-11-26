fe5249cf83b8cf83c699ef2f4b96bc48a2be002e
박지원 <jackgg12322@gmail.com>
2023-11-26 17:09:47 +0900

    refactor : StrictMode 선언

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import NavBar from './Component/NavBar';
 import Header from './Component/Header';
@@ -7,12 +7,13 @@ import Main from './Component/Main';
 import './CSS/Landing.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<>
+  <StrictMode>
     <NavBar/>
     <Header/>
     <Main/>
     <Footer/>
-    </>
+    </StrictMode>
+
 );
 
 
