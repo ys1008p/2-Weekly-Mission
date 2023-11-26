1addb147cd03c59d6ad96777235afc7d00098ade
박지원 <jackgg12322@gmail.com>
2023-11-26 17:27:13 +0900

    feat : CardComponent api 활용 기능 구현

--- my-react-app/src/Component/MainCardContainer.jsx
+++ my-react-app/src/Component/MainCardContainer.jsx
@@ -1,87 +1,64 @@
-import React from "react";
-import CardComponent from "./CardComponent";
-import ImgCard1 from '../images/Card1.png'
-import ImgCard2 from '../images/Card2.png'
-import ImgCard3 from '../images/Card3.png'
-import ImgCard4 from '../images/Card4.png'
-import ImgCard5 from '../images/Card5.png'
-import ImgCard6 from '../images/Card6.png'
-import ImgCard7 from '../images/Card7.png'
-import ImgCard8 from '../images/Card8.png'
-import ImgCard9 from '../images/Card9.png'
+import React, { useEffect, useState } from "react";
+import { LoginProfile } from "../api";
+import NULL_IMG from '../images/logo.svg'
+function timeAgo(timestamp) {
+  const now = new Date();
+  const seconds = Math.floor((now - timestamp) / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
 
-
+  if (seconds < 120) {
+    return "1 minute ago";
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`;
+  } else if (hours < 24) {
+    return "1 hour ago";
+  } else if (days < 30) {
+    return `${days} days ago`;
+  } else if (months < 12) {
+    return `${months} months ago`;
+  } else if (years === 1) {
+    return "1 year ago";
+  } else if (years > 1) {
+    const roundedYears = Math.floor(years);
+    return `${roundedYears} years ago`;
+  }
+  return "Just now";
+}
 
 export default function MainCardContainer() {
-  const TextTime = '10 minutes ago';
-  const TextDescription = `Lorem ipsum dolor sit amet 
-  consectetur. Metus amet habitant nunc 
-  consequat. Tldkd`;
-  const Years = new Date();
-  const TextYears = `${Years.getFullYear()}. ${Years.getMonth() + 1}. ${Years.getDate()}`;
+  const [folderData, setFolderData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const folderResponse = await LoginProfile();
+      setFolderData(folderResponse.folder);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div className="MainCardContainer">
-      <div className="MainCardBox">
-        <CardComponent 
-        img = {ImgCard1}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-        <CardComponent 
-        img = {ImgCard2}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-        <CardComponent 
-        img ={ImgCard3}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-      </div>
-      <div className="MainCardBox">
-      <CardComponent 
-        img ={ImgCard4}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-        <CardComponent 
-        img ={ImgCard5}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-        <CardComponent 
-        img ={ImgCard6}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-      </div>
-      <div className="MainCardBox">
-      <CardComponent 
-        img ={ImgCard7}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-        <CardComponent 
-        img ={ImgCard8}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-        <CardComponent 
-        img ={ImgCard9}
-        time = {TextTime}
-        text = {TextDescription}
-        years = {TextYears}
-        />
-      </div>
+    <div className="CardBoxContainer">
+      {folderData && folderData.links && folderData.links.length > 0 ? (
+        folderData.links.map((item, index) => (
+          <div key={index} className="CardBox">
+            <a href={item.url} target="_blank" alt='targetUrl'>
+              <img src={item.imageSource} alt="이미지" className="CardImg" />
+              <div className="CardTextBox">
+                <p className="CardUpLoadTime">
+                  {timeAgo(new Date(item.createdAt))}
+                </p>
+                <p className="CardTextDescription">{item.description}</p>
+                <p className="CardTextYears">{item.createdAt}</p>
+              </div>
+            </a>
+          </div>
+        ))
+      ) : (
+        <p>{null}</p>
+      )}
     </div>
   );
 }
