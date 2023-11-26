e1ba585dbb736ca46d21826865bb9faf7ccfd190
Chyyni <catharina0409@gmail.com>
2023-11-26 20:38:37 +0900

    refactor: 프로필 이미지 변경

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Profile.css';
-import UserImage from '../images/user.svg';
 
-// 구조분해로 가져오면 왜 자꾸 undefined일까..?
 export default function Profile({ user }) {
   return (
     <div className='user'>
-      <img className='user-image' src={UserImage} alt={user.name} />
+      <img
+        className='user-image'
+        src={user.profileImageSource}
+        alt={user.name}
+      />
       <span className='user-email'>{user.email}</span>
     </div>
   );
