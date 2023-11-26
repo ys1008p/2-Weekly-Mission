5a800a266a8b6038292536775409ed62d3ab0f2b
designsoo <kheesoo10@gmail.com>
2023-11-26 15:20:17 +0900

    refactor: Modify the structure of codeit-symbol to folder-symbol

--- src/components/shareLink/MainBanner.jsx
+++ src/components/shareLink/MainBanner.jsx
@@ -9,12 +9,14 @@ export const MainBanner = () => {
     <section className='folder-banner'>
       <h2 className='visually-hidden'>즐겨찾기 배너</h2>
       <div className='container'>
-        <div className='codeit-symbol'>
-          <img
-            className='codeit-symbol-avatar'
-            src={owner?.profileImageSource || share.emptyAvatar}
-            alt='코드잇 심볼'
-          />
+        <div className='folder-symbol'>
+          <div className='folder-symbol-avatar'>
+            <img
+              className='folder-symbol-avatar-img'
+              src={owner?.profileImageSource || share.emptyAvatar}
+              alt='코드잇 심볼'
+            />
+          </div>
           <span>{owner?.name}</span>
         </div>
         <h3 className='folder-banner-title'>{name}</h3>
