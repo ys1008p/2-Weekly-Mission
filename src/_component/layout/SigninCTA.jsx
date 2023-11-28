0a0b36c5ff71e1fc2bb9226cfb6fc4bcff8b638d
dkile <dkile532@naver.com>
2023-11-28 13:00:17 +0900

    Refactor: SigninCTA 내 경로를 상수로 변경

--- src/_component/layout/SigninCTA.jsx
+++ src/_component/layout/SigninCTA.jsx
@@ -1,11 +1,12 @@
+import { PATH_SIGNIN } from "/src/_constant/routes";
 import CTA from "/src/_component/common/CTA";
 
 export default function SigninCTA() {
   return (
     <CTA
-      to="/auth/signin.html"
+      to={PATH_SIGNIN}
       content="로그인"
-      className="tablet:w-[12.8rem] tablet:px-[2rem] tablet:py-[1.6rem] tablet:text-[1.8rem] flex w-[8rem] items-center justify-center rounded-[0.8rem] bg-gradient-purple-skyblue px-[1.6rem] py-[1rem] text-[1.4rem] font-semibold text-u-white"
+      className="flex w-[8rem] items-center justify-center rounded-[0.8rem] bg-gradient-purple-skyblue px-[1.6rem] py-[1rem] text-[1.4rem] font-semibold text-u-white tablet:w-[12.8rem] tablet:px-[2rem] tablet:py-[1.6rem] tablet:text-[1.8rem]"
     />
   );
 }
