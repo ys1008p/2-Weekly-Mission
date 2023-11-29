96d6363caa2471ff0d50d8511d6f01f54c09b515
dkile <dkile532@naver.com>
2023-11-29 21:14:44 +0900

    Feat: React Router 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SharedPage from "@/pages/SharedPage";
 import "./globals.css";
 import AuthProvider from "@/providers/AuthProvider";
 import UserProvider from "@/providers/UserProvider";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import SharedPage from "@/pages/SharedPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/shared",
+    element: <SharedPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <UserProvider>
-        <SharedPage />
+        <RouterProvider router={router} />
       </UserProvider>
     </AuthProvider>
   </React.StrictMode>,
