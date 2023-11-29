d234685b111f6111b6490b64b2baeba4c4fb2163
So-Yeon Ahn <sozygn@gmail.com>
2023-11-29 17:08:30 +0900

    fix: hover 이벤트 스타일 pseudo class로 처리, hover 테두리에 내부 content 공간 밀리는 현상 해결

--- src/components/Cards/CardItem.js
+++ src/components/Cards/CardItem.js
@@ -1,7 +1,6 @@
 import Stars from '../Stars/Stars';
 import logoImg from '../Gnb/logo.svg';
 import { timeAgo, getYYYYMMYY, getShortDescription } from '../../utils/formatting';
-import { useState } from 'react';
 
 function EmptyCard() {
   return (
@@ -12,42 +11,24 @@ function EmptyCard() {
 }
 
 export default function CardItem({ link }) {
-  const [isHovering, setIsHovering] = useState(false);
-
-  const handleMouseOver = () => {
-    setIsHovering(true);
-  };
-
-  const handleMouseOut = () => {
-    setIsHovering(false);
-  };
-
   return (
-    <div className="CardItem">
-      <a
-        className={isHovering ? 'contentBox hoveredCard' : 'contentBox'}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        href={link.url}
-        target="_blank"
-      >
-        <div class="imgContainer">
-          {link.imageSource ? (
-            <img className={isHovering ? 'contentImg hoveredImg' : 'contentImg'} src={link.imageSource}></img>
-          ) : (
-            <EmptyCard className={isHovering ? 'hoveredImg' : ''} />
-          )}
-          <Stars />
+    <a className="contentBox" href={link.url} target="_blank">
+      <div class="imgContainer">
+        {link.imageSource ? (
+          <img className="contentImage" src={link.imageSource}></img>
+        ) : (
+          <EmptyCard className="hoveredImg" />
+        )}
+        <Stars />
+      </div>
+      <section className="contentText">
+        <div className="contentNav">
+          <div className="timeCreated">{timeAgo(link.createdAt)}</div>
+          <div className="moreInfoCebap"></div>
         </div>
-        <section className="contentText">
-          <div className="contentNav">
-            <div className="timeCreated">{timeAgo(link.createdAt)}</div>
-            <div className="moreInfoCebap"></div>
-          </div>
-          <div className="description">{getShortDescription(link.description)}</div>
-          <div className="dateCreated">{getYYYYMMYY(link.createdAt)}</div>
-        </section>
-      </a>
-    </div>
+        <div className="description">{getShortDescription(link.description)}</div>
+        <div className="dateCreated">{getYYYYMMYY(link.createdAt)}</div>
+      </section>
+    </a>
   );
 }
