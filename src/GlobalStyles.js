82e86b697b314d03a1445700f84400fd0d085e3e
김우현 <rladngus133@naver.com>
2023-11-30 07:13:12 +0900

    fix: box-sizing 적용안됨 해결

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,14 +3,21 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 ${reset};
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
 html{
     font-size: 62.5%;
-    box-sizing: border-box;
 }
 
 body {
     font-family: "Pretendard Variable", Arial, sans-serif;
 }
+
 `;
 
 export default GlobalStyle;
