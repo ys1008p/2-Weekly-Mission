00bd206591914fbbaae786665a56d395b90334a3
So-Yeon Ahn <sozygn@gmail.com>
2023-11-29 16:52:07 +0900

    refactor: CardItem과 Cards 분리 및 그 외 자잘한 수정

---
+++ src/components/Cards/CardItem.js
@@ -0,0 +1,53 @@
+import Stars from '../Stars/Stars';
+import logoImg from '../Gnb/logo.svg';
+import { timeAgo, getYYYYMMYY, getShortDescription } from '../../utils/formatting';
+import { useState } from 'react';
+
+function EmptyCard() {
+  return (
+    <div className="EmptyCard">
+      <img className="logoInCard" src={logoImg}></img>
+    </div>
+  );
+}
+
+export default function CardItem({ link }) {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
+  return (
+    <div className="CardItem">
+      <a
+        className={isHovering ? 'contentBox hoveredCard' : 'contentBox'}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        href={link.url}
+        target="_blank"
+      >
+        <div class="imgContainer">
+          {link.imageSource ? (
+            <img className={isHovering ? 'contentImg hoveredImg' : 'contentImg'} src={link.imageSource}></img>
+          ) : (
+            <EmptyCard className={isHovering ? 'hoveredImg' : ''} />
+          )}
+          <Stars />
+        </div>
+        <section className="contentText">
+          <div className="contentNav">
+            <div className="timeCreated">{timeAgo(link.createdAt)}</div>
+            <div className="moreInfoCebap"></div>
+          </div>
+          <div className="description">{getShortDescription(link.description)}</div>
+          <div className="dateCreated">{getYYYYMMYY(link.createdAt)}</div>
+        </section>
+      </a>
+    </div>
+  );
+}
--- src/components/Gnb/Gnb.js
+++ src/components/Gnb/Gnb.js
@@ -3,18 +3,16 @@ import './Gnb.css';
 import Profile from '../Profile/Profile';
 import LoginButton from '../LoginButton/LoginButton';
 function Gnb({ profile }) {
-  const didLogin = profile.id ? true : false;
+  const didLogin = !!profile.id;
   return (
-    <>
-      <nav className="navContainer">
-        <div className="nav">
-          <a href="index.html">
-            <img src={logoImg} className="logo-img" alt="홈으로 연결된 Linkbrary 로고" />
-          </a>
-          {didLogin ? <Profile profile={profile} /> : <LoginButton />}
-        </div>
-      </nav>
-    </>
+    <nav className="navContainer">
+      <div className="nav">
+        <a href="index.html">
+          <img src={logoImg} className="logo-img" alt="홈으로 연결된 Linkbrary 로고" />
+        </a>
+        {didLogin ? <Profile profile={profile} /> : <LoginButton />}
+      </div>
+    </nav>
   );
 }
 
