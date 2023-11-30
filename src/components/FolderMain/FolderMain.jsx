c69538dee80d0983efafaf6459eb7e6dad0d2aee
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-30 16:28:16 +0900

    test: folder 페이지 초기 템플릿 구현

--- src/components/FolderMain/FolderMain.jsx
+++ src/components/FolderMain/FolderMain.jsx
@@ -1,18 +1,54 @@
 import './FolderMain.css';
 import SearchBar from '../atoms/SearchBar/SearchBar';
-
+import LinkIcon from '../../assets/link.svg';
+import CardList from '../CardList/CardList';
+import Folder from '../../folder-mock.json';
+import FolderList from '../../list-mock.json';
 function FolderMain() {
+  const { folder } = Folder;
+  const { data } = FolderList;
+
   return (
     <main>
-      <div>
-        <form>
-          <input type="text" />
-          <button>추가하기</button>
+      <div className="LinkInput">
+        <form className="LinkInput-form">
+          <img src={LinkIcon} alt="링크" className="LinkInput-icon" />
+          <input type="text" placeholder="링크를 추가해 보세요" className="LinkInput-input" />
+          <button type="button" className="Button">
+            추가하기
+          </button>
         </form>
       </div>
-      <div>
+
+      <article className="test">
         <SearchBar />
-      </div>
+        <div>
+          <ul>
+            {data.map((item) => {
+              return <li key={item.id}>{item.name}</li>;
+            })}
+          </ul>
+          <div>+</div>
+        </div>
+        <div>
+          <div>유용한 글</div>
+          <div>
+            <div>
+              <img src="" />
+              <span>공유</span>
+            </div>
+            <div>
+              <img src="" />
+              <span>이름 변경</span>
+            </div>
+            <div>
+              <img src="" />
+              <span>삭제</span>
+            </div>
+          </div>
+        </div>
+        <CardList links={folder.links} />
+      </article>
     </main>
   );
 }
