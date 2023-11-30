f5affbb5568d765e18156757b5df5444969dae06
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-30 14:15:14 +0900

    add: folder 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shared from './pages/Shared';
+import FolderPage from './pages/FolderPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route index path="/shared" element={<Shared />} />
+          <Route path="/shared" element={<Shared />} />
+          <Route path="/folder" element={<FolderPage />} />
           <Route path="*" element={<div>잘못된 페이지입니다.</div>} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/FolderPage.jsx
@@ -0,0 +1,11 @@
+import Layout from './Layout';
+
+function FolderPage() {
+  return (
+    <Layout>
+      <div>레이아웃 테스트임</div>
+    </Layout>
+  );
+}
+
+export default FolderPage;
