703233716b113020e169d29bd7062792e830ccff
Chyyni <catharina0409@gmail.com>
2023-11-30 18:31:55 +0900

    refactor: 반복되는 sns 데이터 상수데이터로 만들기

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,93 +6,65 @@ import Youtube from '../images/youtube.svg';
 import './Footer.css';
 
 export default function Footer() {
+  const snsList = SNSList.map(({ title, link, imageSrc, altMessage }) => (
+    <li key={title}>
+      <SNS link={link} imageSrc={imageSrc} altMessage={altMessage} />
+    </li>
+  ));
+
   return (
     <div className='footer'>
       <div className='footer-container'>
-        <Copyright>{`©codeit - 2023`}</Copyright>
-        <Policy />
-        <SNS />
+        <p className='copyright'>©codeit - 2023</p>
+        <ul className='policy-links'>
+          <li>
+            <a className='policy-link' href='/privacy'>
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a className='policy-link' href='/faq'>
+              FAQ
+            </a>
+          </li>
+        </ul>
+        <ul className='sns-container'>{snsList}</ul>
       </div>
     </div>
   );
 }
 
-function Copyright({ children }) {
-  return <p className='copyright'>{children}</p>;
-}
-
-function Policy() {
+function SNS({ link, imageSrc, altMessage }) {
   return (
-    <ul className='policy-links'>
-      <li>
-        <a className='policy-link' href='/privacy'>
-          Privacy Policy
-        </a>
-      </li>
-      <li>
-        <a className='policy-link' href='/faq'>
-          FAQ
-        </a>
-      </li>
-    </ul>
+    <a href={link} target='_blank' rel='noreferrer noopener'>
+      <img className='sns-icon' src={imageSrc} alt={altMessage} />
+    </a>
   );
 }
 
-function SNS() {
-  return (
-    <ul className='sns-container'>
-      <li>
-        <a
-          href='https://www.facebook.com/'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          <img
-            className='sns-icon'
-            src={Facebook}
-            alt='페이스북 홈페이지로 이동하는 페이스북 아이콘'
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href='https://twitter.com/'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          <img
-            className='sns-icon'
-            src={Twitter}
-            alt='트위터 홈페이지로 이동하는 트위터 아이콘'
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href='https://www.youtube.com/'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          <img
-            className='sns-icon'
-            src={Youtube}
-            alt='유튜브 홈페이지로 이동하는 유튜브 아이콘'
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href='https://www.instagram.com/'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          <img
-            className='sns-icon'
-            src={Instagram}
-            alt='인스타그램 홈페이지로 이동하는 인스타그램 아이콘'
-          />
-        </a>
-      </li>
-    </ul>
-  );
-}
+const SNSList = [
+  {
+    title: 'facebook',
+    link: 'https://www.facebook.com/',
+    imageSrc: Facebook,
+    altMessage: '페이스북 홈페이지로 이동하는 페이스북 아이콘',
+  },
+  {
+    title: 'twitter',
+    link: 'https://www.twitter.com/',
+    imageSrc: Twitter,
+    altMessage: '트위터 홈페이지로 이동하는 트위터 아이콘',
+  },
+  {
+    title: 'youtube',
+    link: 'https://www.youtube.com/',
+    imageSrc: Youtube,
+    altMessage: '유튜브 홈페이지로 이동하는 유튜브 아이콘',
+  },
+  {
+    title: 'instagram',
+    link: 'https://www.instagram.com/',
+    imageSrc: Instagram,
+    altMessage: '인스타그램 홈페이지로 이동하는 인스타그램 아이콘',
+  },
+];
