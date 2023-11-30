809669344b7ab276619e28ff7a0ac23637892f47
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-30 14:05:07 +0900

    feature: 리액트 라우터를 사용해 /shared 페이지 라우트 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shared from './pages/Shared';
 
 function App() {
   return (
     <div className="App">
-      <Shared />
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/shared" element={<Shared />} />
+          <Route path="*" element={<div>잘못된 페이지입니다.</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
