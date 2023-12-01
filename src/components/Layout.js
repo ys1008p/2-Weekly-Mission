b93524a835e2db727d5b8246afb5ed8c45706e8e
So-Yeon Ahn <sozygn@gmail.com>
2023-12-01 10:39:50 +0900

    feat: 기본 Layout 구성에 AddLinkBar 추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
-import Gnb from './shared/Gnb/Gnb';
-import Footer from './shared/Footer/Footer';
 import { getUser } from './fetchApi';
 import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Gnb from './shared/Gnb/Gnb';
+import Footer from './shared/Footer/Footer';
+import SearchBar from './shared/SearchBar/SearchBar';
+import AddLinkBar from './shared/AddLinkBar/AddLinkBar';
 
-// Gnb, Footer 갖춘 기본 레이아웃
+// Gnb, AddLinkBar, SearchBar, Footer 갖춘 기본 레이아웃
 function Layout() {
   const [profile, setProfile] = useState({
     id: 0,
@@ -25,6 +27,8 @@ function Layout() {
   return (
     <>
       <Gnb profile={profile} />
+      <AddLinkBar />
+      <SearchBar />
       <Outlet />
       <Footer />
     </>
