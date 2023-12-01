85b34aa54e3cffb0b2592865ddc09b12c4a4755c
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-01 11:57:27 +0900

    feature: 탭 클릭시 스타일 바뀌는 기능 구현

--- src/components/atoms/FolderList/FolderList.jsx
+++ src/components/atoms/FolderList/FolderList.jsx
@@ -1,15 +1,38 @@
 import styles from './FolderList.module.css';
 import AddImg from '../../../assets/add.svg';
+import { useState } from 'react';
+
 function FolderList({ data }) {
+  const [activeKey, setActive] = useState('all');
+  const handleClick = (e) => {
+    setActive(e.target.id);
+  };
+
   return (
     <div className={styles.FolderList}>
       <ul className={styles.list}>
-        <li key={0} className={`${styles.listItem} ${styles.active}`}>
+        <li
+          key={0}
+          id="all"
+          className={
+            activeKey === 'all' ? `${styles.listItem} ${styles.active}` : `${styles.listItem}`
+          }
+          onClick={handleClick}
+        >
           전체
         </li>
         {data.map((item) => {
           return (
-            <li key={item.id} className={`${styles.listItem}`}>
+            <li
+              key={item.id}
+              id={item.id}
+              className={
+                String(item.id) === activeKey
+                  ? `${styles.listItem} ${styles.active}`
+                  : `${styles.listItem}`
+              }
+              onClick={handleClick}
+            >
               {item.name}
             </li>
           );
