695de7faf07c8bc02f7de8efa25cdb9d727c7ba0
Yoon-kyungLee <yk2588@nate.com>
2023-12-01 22:16:58 +0900

    feat: 초기 화면 folder 페이지로 리다이렉트

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SharedPage from "./pages/Shared/SharedPage";
 import FolderPage from "./pages/Folder/FolderPage";
 
@@ -6,6 +6,7 @@ function Main() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/folder" />} />
         <Route path="/shared" element={<SharedPage />} />
         <Route path="/folder" element={<FolderPage />} />
       </Routes>
