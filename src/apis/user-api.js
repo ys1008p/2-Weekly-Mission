0aea7484a6ab189ea44e6445ff849b050fcd11e4
dkile <dkile532@naver.com>
2023-12-01 10:33:22 +0900

    Feat: 404에러 추가

--- src/apis/user-api.js
+++ src/apis/user-api.js
@@ -1,11 +1,15 @@
-import { UnauthorizedError } from "@/utils/common/FetchError";
+import { UnAuthorizedError } from "@/utils/common/FetchError";
 import Fetcher from "@/utils/common/Fetcher";
 
 const fetcher = new Fetcher({
   baseURL: import.meta.env.VITE_API_ENDPOINT,
   handlers: [
     (res) => {
-      if (res.status === 401) throw new UnauthorizedError();
+      if (res.status === 401) throw new UnAuthorizedError();
+      return res;
+    },
+    (res) => {
+      if (res.status === 404) throw new APINotFoundError();
       return res;
     },
   ],
--- src/utils/common/FetchError.js
+++ src/utils/common/FetchError.js
@@ -1,6 +1,7 @@
 const ERROR_MESSAGE = {
   UNAUTHORIZED_ERROR: "인증되지 않은 사용자입니다.",
   FORBIDDEN_ERROR: "접근 권한이 없는 사용자입니다.",
+  API_NOT_FOUND_ERROR: "해당 API를 찾을 수 없습니다.",
 };
 
 export class FetchError extends Error {
@@ -13,9 +14,16 @@ export class FetchError extends Error {
   }
 }
 
-export class UnauthorizedError extends FetchError {
+export class UnAuthorizedError extends FetchError {
   constructor() {
     super(ERROR_MESSAGE.UNAUTHORIZED_ERROR);
     this.name = "Unauthorized user";
+  }
+}
+
+export class APINotFoundError extends FetchError {
+  constructor() {
+    super(ERROR_MESSAGE.API_NOT_FOUND_ERROR);
+    this.name = "API Not found";
   }
 }
