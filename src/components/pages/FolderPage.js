18969e9cb2850cdf09b273758616e80e00d9a506
grapefruit13 <liam.heo11@gmail.com>
2023-12-02 19:34:52 +0900

    Feat: 7주차-api 요청 page 컴포넌트에서 처리하도록 수정

--- src/components/pages/FolderPage.js
+++ src/components/pages/FolderPage.js
@@ -1,13 +1,46 @@
-import { Main } from "../main/Main.js";
+import { FolderMain } from "../main/FolderMain.js";
 import { Footer } from "../footer/Footer.js";
 import AddLink from "../header/AddLink.js";
-import FolderHeader from "../header/FolderHeader.js";
+import Header from "../header/Header.js";
+import { useEffect, useState } from "react";
+import { getData } from "../api/api";
 function FolderPage() {
+  const [profileDatas, setProfileDatas] = useState({
+    id: 0,
+    created_at: "",
+    name: "",
+    image_source: "",
+    email: "",
+    auth_id: "",
+  });
+
+  const getUserData = async () => {
+    try {
+      const result = await getData("users/1");
+      const { id, created_at, name, image_source, email, auth_id } =
+        result.data[0];
+
+      setProfileDatas((prevProfileDatas) => ({
+        ...prevProfileDatas,
+        id: id,
+        created_at: created_at,
+        name: name,
+        image_source: image_source,
+        email: email,
+        auth_id: auth_id,
+      }));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
   return (
     <>
-      <FolderHeader />
+      <Header profileDatas={profileDatas} />
       <AddLink />
-      <Main />
+      <FolderMain />
       <Footer />
     </>
   );
--- src/components/pages/SharedPage.js
+++ src/components/pages/SharedPage.js
@@ -1,11 +1,40 @@
 import Header from "../header/Header.js";
 import Footer from "../footer/Footer.js";
 import Main from "../main/Main.js";
+import { useEffect, useState } from "react";
+import { getData } from "../api/api";
 
 function SharedPage() {
+  const [profileDatas, setProfileDatas] = useState({
+    id: 0,
+    name: "",
+    email: "",
+    profileImageSource: "",
+  });
+
+  const getSampleUserData = async () => {
+    try {
+      const response = await getData("sample/user");
+      // console.log(response, "Header");
+      const { id, name, email, profileImageSource } = response;
+      setProfileDatas((prevProfileDatas) => ({
+        ...prevProfileDatas,
+        id: id,
+        name: name,
+        email: email,
+        profileImageSource: profileImageSource,
+      }));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getSampleUserData();
+  }, []);
   return (
     <>
-      <Header />
+      <Header profileDatas={profileDatas} />
       <Main />
       <Footer />
     </>
