3a40beb91ccb581607c1b93ef757831df241dd3d
grapefruit13 <liam.heo11@gmail.com>
2023-12-02 19:34:15 +0900

    Feat: 7주차-header 컴포넌트 하나로 통합

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,37 +1,7 @@
-import { useEffect, useState } from "react";
-import "./Header.css";
-import logo from "../../assets/logo.png";
-import { getData } from "../api/api";
 import LoginButton from "./LoginButton";
 import Logo from "./Logo";
-export function Header() {
-  const [profileDatas, setProfileDatas] = useState({
-    id: 0,
-    name: "",
-    email: "",
-    profileImageSource: "",
-  });
-
-  const getUserData = async () => {
-    try {
-      const response = await getData("sample/user");
-      // console.log(response, "Header");
-      const { id, name, email, profileImageSource } = response;
-      setProfileDatas((prevProfileDatas) => ({
-        ...prevProfileDatas,
-        id: id,
-        name: name,
-        email: email,
-        profileImageSource: profileImageSource,
-      }));
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    getUserData();
-  }, []);
+import "./Header.css";
+export function Header({ profileDatas }) {
   return (
     <div className="header">
       <nav>
@@ -40,14 +10,11 @@ export function Header() {
           <div className="header-profile">
             <img
               className="header-profile_profile-img"
-              src={profileDatas.profileImageSource}
+              src={profileDatas.profileImageSource || profileDatas.image_source}
             />
             <p className="header-profile_profile-name">{profileDatas.email}</p>
           </div>
         ) : (
-          // <a href="signin.html">
-          //   <button class="login-button button">로그인</button>
-          // </a>
           <LoginButton />
         )}
       </nav>
