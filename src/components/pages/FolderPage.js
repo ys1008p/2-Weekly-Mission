05f606ed5049478fe303118356e019cfa048f169
grapefruit13 <liam.heo11@gmail.com>
2023-12-02 23:44:08 +0900

    Feat: 7주차-폴더 아이디별로 링크 보여주는 기능 구현

--- src/components/pages/FolderPage.js
+++ src/components/pages/FolderPage.js
@@ -20,6 +20,8 @@ function FolderPage() {
 
   const [links, setLinks] = useState([]);
   const [folderLists, setFolderLists] = useState([]);
+  const [currentFolderId, setCurrentFolderId] = useState("");
+
   const getUserData = async () => {
     try {
       const result = await getData("users/1");
@@ -59,17 +61,51 @@ function FolderPage() {
     try {
       const result = await getData("users/1/folders");
       const { data } = result;
+      console.log(data);
+      data.unshift({ name: "전체" });
       setFolderLists(data);
     } catch (e) {
       console.log(`Folderpage의 getFolderLists에서 ${e} 발생`);
     }
   };
 
+  const getFolder = async () => {
+    try {
+      console.log("id", currentFolderId);
+      const result = await getData(`users/1/links?folderId=${currentFolderId}`);
+      console.log("특정 폴더", result);
+      const { data } = result;
+      console.log("data", data);
+      const datas = data.map((link) => ({
+        ...link,
+        createdAt: link.created_at,
+        imageSource: link.image_source,
+      }));
+      setLinks(datas);
+    } catch (e) {
+      console.log(`Folderpage의 handleFolderClick의 getFolder에서 ${e} 발생`);
+    }
+  };
+  const handleFolderClick = (e) => {
+    console.log(e.target.textContent);
+    if (e.target.textContent === "전체") {
+      getTotalLinksData();
+      return;
+    }
+    const clikedFolder = folderLists.filter(
+      (folder) => folder.name === e.target.textContent
+    );
+    setCurrentFolderId(clikedFolder[0].id);
+  };
   useEffect(() => {
     getUserData();
     getTotalLinksData();
     getFolderLists();
   }, []);
+
+  useEffect(() => {
+    getFolder();
+  }, [currentFolderId]);
   return (
     <>
       <Header profileDatas={profileDatas} />
@@ -79,7 +115,7 @@ function FolderPage() {
         {folderLists ? (
           folderLists.map((folder) => {
             return (
-              <div key={folder.id}>
+              <div key={folder.id} onClick={handleFolderClick}>
                 <Folders folder={folder} />
               </div>
             );
