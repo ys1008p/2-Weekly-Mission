84d81929158cdd9129836a2c40fbb99ebcefca4e
CheeseB <2489ckckck@naver.com>
2023-12-02 23:26:20 +0900

    refactor(mentor): 시간차 계산하는 유틸함수 리팩토링

--- src/utils/getTimeDiff.ts
+++ src/utils/getTimeDiff.ts
@@ -5,6 +5,14 @@ const DAY_IN_SECOND = 24 * HOUR_IN_SECOND;
 const MONTH_IN_SECOND = 30 * DAY_IN_SECOND;
 const YEAR_IN_SECOND = 12 * MONTH_IN_SECOND;
 
+const timeUnits = [
+  { unit: 'year', seconds: YEAR_IN_SECOND },
+  { unit: 'month', seconds: MONTH_IN_SECOND },
+  { unit: 'day', seconds: DAY_IN_SECOND },
+  { unit: 'hour', seconds: HOUR_IN_SECOND },
+  { unit: 'minute', seconds: MINUTE_IN_SECOND },
+];
+
 const getTimeDiffInSeconds = (dateTime: string): number => {
   const date = new Date(dateTime);
   const now = new Date();
@@ -15,32 +23,14 @@ const getTimeDiffInSeconds = (dateTime: string): number => {
 const getTimeDiff = (dateTime: string): string => {
   const timeDiffSeconds = getTimeDiffInSeconds(dateTime);
 
-  if (timeDiffSeconds < MINUTE_IN_SECOND) {
-    return '1 minute ago';
-  } else if (timeDiffSeconds <= 59 * MINUTE_IN_SECOND) {
-    const minutes = Math.floor(timeDiffSeconds / MINUTE_IN_SECOND);
-    return `${minutes} minutes ago`;
-  } else if (timeDiffSeconds <= HOUR_IN_SECOND) {
-    return '1 hour ago';
-  } else if (timeDiffSeconds <= 23 * HOUR_IN_SECOND) {
-    const hours = Math.floor(timeDiffSeconds / HOUR_IN_SECOND);
-    return `${hours} hours ago`;
-  } else if (timeDiffSeconds <= DAY_IN_SECOND) {
-    return '1 day ago';
-  } else if (timeDiffSeconds <= 29 * DAY_IN_SECOND) {
-    const days = Math.floor(timeDiffSeconds / DAY_IN_SECOND);
-    return `${days} days ago`;
-  } else if (timeDiffSeconds <= MONTH_IN_SECOND) {
-    return '1 month ago';
-  } else if (timeDiffSeconds <= 11 * MONTH_IN_SECOND) {
-    const months = Math.floor(timeDiffSeconds / MONTH_IN_SECOND);
-    return `${months} months ago`;
-  } else if (timeDiffSeconds <= YEAR_IN_SECOND) {
-    return '1 year ago';
-  } else {
-    const years = Math.floor(timeDiffSeconds / YEAR_IN_SECOND);
-    return `${years} years ago`;
+  for (const { unit, seconds } of timeUnits) {
+    if (timeDiffSeconds < seconds) continue;
+
+    const count = Math.floor(timeDiffSeconds / seconds);
+    return `${count} ${unit}${count > 1 ? 's' : ''} ago`;
   }
+
+  return '1 minute ago';
 };
 
 export default getTimeDiff;
