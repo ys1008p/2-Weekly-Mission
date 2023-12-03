6ec153bf6e162d3896e29d3a6a62636a4ab49883
Chyyni <catharina0409@gmail.com>
2023-12-03 19:32:18 +0900

    feat: 버튼 클릭 시 해당 폴더 링크 보여주기 (오류 해결)

--- src/components/FolderCardList.jsx
+++ src/components/FolderCardList.jsx
@@ -4,15 +4,22 @@ import CardList from './CardList';
 import EmptyCardList from './EmptyCardList';
 
 export default function FolderCardList({ selectedFolder }) {
-  const { folderId, folderName } = selectedFolder;
+  const { id: folderId, name: folderName } = selectedFolder;
   const query = folderId ? `?folderId=${folderId}` : '';
-  const [loading, error, linkData] = useGetData(`users/1/links${query}`);
+
+  const [loading, error, linkData] = useGetData(
+    `users/1/links${query}`,
+    selectedFolder
+  );
 
   if (loading) return <div>잠시만 기다려 주세요.</div>;
   if (error) return <p>저장된 링크를 찾을 수 없습니다.</p>;
 
   const links = linkData?.data;
   console.log(links);
+
+  if (loading) return <p>loading..</p>;
+  if (error) return <EmptyCardList />;
 
   return (
     <div>
