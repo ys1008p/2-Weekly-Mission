2d4c9050c3e63f2de52fea9698976c29148586fa
So-Yeon Ahn <sozygn@gmail.com>
2023-12-03 15:46:16 +0900

    feat: 태블릿, PC 버전 폴더추가버튼 제작

--- src/components/others/Sorting/Sorting.js
+++ src/components/others/Sorting/Sorting.js
@@ -1,7 +1,13 @@
 import './Sorting.css';
 import FolderButton from '../../shared/FolderButton/FolderButton';
+import addFolderIcon from './add.png';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Sorting({ chosenFolderId, folder, handleQuery }) {
+  const isNotMobile = useMediaQuery({
+    query: '(min-width :768px)',
+  });
+
   const allInfo = {
     id: undefined,
     name: '전체',
@@ -9,24 +15,32 @@ export default function Sorting({ chosenFolderId, folder, handleQuery }) {
   const needAllLink = chosenFolderId === allInfo.id ? true : false;
 
   return (
-    <div className="Sorting">
-      <FolderButton
-        isFolderChosen={needAllLink}
-        chosenFolderId={chosenFolderId}
-        folderInfo={allInfo}
-        handleQuery={handleQuery}
-      />
-      {folder.map((folderInfo) => {
-        const isFolderChosen = +chosenFolderId === folderInfo.id ? true : false; // 선택된 폴더인지 확인
-        return (
-          <FolderButton
-            isFolderChosen={isFolderChosen}
-            key={folderInfo.id}
-            folderInfo={folderInfo}
-            handleQuery={handleQuery}
-          />
-        );
-      })}
+    <div className="folderNav">
+      <div className="Sorting">
+        <FolderButton
+          isFolderChosen={needAllLink}
+          chosenFolderId={chosenFolderId}
+          folderInfo={allInfo}
+          handleQuery={handleQuery}
+        />
+        {folder.map((folderInfo) => {
+          const isFolderChosen = +chosenFolderId === folderInfo.id ? true : false; // 선택된 폴더인지 확인
+          return (
+            <FolderButton
+              isFolderChosen={isFolderChosen}
+              key={folderInfo.id}
+              folderInfo={folderInfo}
+              handleQuery={handleQuery}
+            />
+          );
+        })}
+      </div>
+      {isNotMobile && (
+        <div className="addFolderButton">
+          <div className="folderAddText">폴더 추가</div>
+          <img className="addFolderIcon" src={addFolderIcon}></img>
+        </div>
+      )}
     </div>
   );
 }
