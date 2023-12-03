2aa83b8e64e80e8c6aa04f4dfb9b723982060a31
caseV12 <caseVDev@gmail.com>
2023-12-03 18:10:29 +0900

    Feat: Custom hook useFetch.js 작성

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (resource, opts = {}) => {
+  if (!resource){
+    return;
+  }
+  const [state, setState] = useState({
+   loading: true,
+   error: false,
+   data: null, 
+  });
+  const [trigger, setTrigger] = useState(0);
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now())
+  }
+  useEffect(() => {
+    fetch(resource, opts)
+      .then(data => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        })
+      })
+      .catch(error => {
+        setState({...state, loading: false, error})
+      })
+  }, [trigger])
+
+  return {...state, refetch}
+}
 
 
-
+export default useFetch;
