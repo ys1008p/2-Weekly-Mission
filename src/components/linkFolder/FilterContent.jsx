4fce5d14061c543cb6c9a49b7dfd2ead02915e70
designsoo <kheesoo10@gmail.com>
2023-12-03 13:42:13 +0900

    feat: Implement FilterContent component

---
+++ src/components/linkFolder/FilterContent.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useStoredData } from '../../utils/useStoredData';
+import { FolderContext } from '../../contexts/FolderProvider';
+
+import { GridLayout } from './GridLayout';
+import { Filter } from '../common/Filter';
+import { FilterOptions } from '../common/FilterOptions';
+
+import { EmptyContent } from '../common/EmptyContent';
+
+export const FilterContent = ({ currentId, setCurrentId }) => {
+  const folderData = useStoredData(FolderContext);
+  const [title, setTitle] = useState('전체');
+
+  return (
+    <div className='filter-content'>
+      <Filter setCurrentId={setCurrentId} setTitle={setTitle} />
+
+      <header className='filter-content-header'>
+        <h3>{title}</h3>
+        {currentId && <FilterOptions />}
+      </header>
+
+      {Object.keys(folderData).length === 0 ? (
+        <EmptyContent />
+      ) : (
+        <GridLayout data={folderData} />
+      )}
+    </div>
+  );
+};
+
+FilterContent.propTypes = {
+  currentId: PropTypes.number,
+  setCurrentId: PropTypes.func,
+};
