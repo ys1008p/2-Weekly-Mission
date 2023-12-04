f8ca2b2733592f5305f8ba159e4e45fc1b5945cc
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2023-12-05 04:33:39 +0900

    feat: FolderUser.js 스타일 컴포넌트 적용

--- src/components/FolderUser/FolderUser.js
+++ src/components/FolderUser/FolderUser.js
@@ -1,4 +1,44 @@
-import './FolderUser.css';
+import styled from 'styled-components';
+
+const FolderInfo = styled.div`
+  padding: 2rem 0 6rem 0;
+  text-align: center;
+  background-color: var(--color-sky-blue);
+
+  @media screen and (min-width:375px) and (max-width:768px) {
+    padding: 1rem 0 4rem 0;
+  }
+
+  img {
+    width: 6rem;
+
+    @media screen and (min-width:375px) and (max-width:768px) {
+      width: 4rem;
+  }
+}
+`
+
+const UserName = styled.p`
+  margin: 1.2rem 0 2rem 0 ;
+  font-size: 1.6rem;
+
+  @media screen and (min-width:375px) and (max-width:768px) {
+    margin: 0.6rem 0 1rem;
+    font-size: 1.4rem;
+    line-height: 1.6rem;
+  }
+`
+
+const Name = styled.p`
+  font-size: 4rem;
+  line-height: 4.8rem;
+  font-weight: 600;
+
+  @media screen and (min-width:375px) and (max-width:768px) {
+    font-size: 3.2rem;
+    line-height: 3.8rem;
+  }
+`
 
 function FolderUser({ 
   folderName, 
@@ -6,15 +46,11 @@ function FolderUser({
   folderUserProfile 
 }){
   return (
-    <div className="folder-info">
-      <img className="user-profile" src={folderUserProfile} alt="폴더 사용자 프로필 이미지" />
-      <p className="user-name">
-        {folderUserName}
-      </p>
-      <p className="name">
-        {folderName}
-      </p>
-    </div>
+    <FolderInfo>
+      <img src={folderUserProfile} alt="폴더 사용자 프로필 이미지" />
+      <UserName>{folderUserName}</UserName>
+      <Name>{folderName}</Name>
+    </FolderInfo>
   );
 }
 
