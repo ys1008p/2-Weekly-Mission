12b56f73ac704f35a94d6fc18f28edb2a0568af4
CheeseB <2489ckckck@naver.com>
2023-12-04 23:05:43 +0900

    feat: 폴더 페이지 레이아웃 구성

--- src/pages/Folder.tsx
+++ src/pages/Folder.tsx
@@ -1,4 +1,7 @@
+import styles from './Folder.module.css';
+
 import FolderBanner from '@/components/banner/FolderBanner';
+import SearchBar from '@/components/input/SearchBar';
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 
@@ -8,6 +11,11 @@ const Folder = () => {
       <Header sticky={false} />
       <main>
         <FolderBanner />
+        <div className={styles.wrapper}>
+          <section className={styles.container}>
+            <SearchBar placeholder="링크를 검색해 보세요." />
+          </section>
+        </div>
       </main>
       <Footer />
     </>
