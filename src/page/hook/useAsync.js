e9ab04465767a2a199e7699930a11540a1776753
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2023-12-05 03:45:41 +0900

    feat: Nav.js 스타일 컴포넌트 적용

--- src/page/hook/useAsync.js
+++ src/page/hook/useAsync.js
@@ -2,7 +2,7 @@ function useAsync(baseUrl, folderId, path, userId){
   const fetchUrl = async () => {
     try{
       const response = await fetch(`https://bootcamp-api.codeit.kr/api${baseUrl}${folderId}${path}${userId}`);
-console.log(response)
+
       if(!response.ok) throw new Error('데이터를 불러오는데 실패했습니다');
 
       const result = await response.json();
--- src/page/shared/nav/Nav.js
+++ src/page/shared/nav/Nav.js
@@ -1,30 +1,106 @@
-import './Nav.css';
 import logo from '../../../assets/logo.svg';
+import styled from 'styled-components';
+
+const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 10;
+  width: 100%;
+  height: 9.4rem;
+  padding: 0 20rem; 
+  background-color: var(--color-sky-blue);
+
+  @media screen and (max-width:1124px) {
+    padding: 0 3.2rem;
+  }
+
+  @media screen and (min-width:375px) and (max-width:768px) {
+    padding: 0 3.2rem; 
+    height: 6.3rem;
+  }
+`;
+
+const Logo = styled.a`
+  display: block;
+
+ img {
+  @media screen and (min-width:375px) and (max-width:768px) {
+    width: 8.8rem;
+  }
+ }
+}
+` 
+
+const Email = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 0 0 0 0.6rem;
+  font-size: 1.4rem;
+  line-height: 1.6rem;
+  color: var(--color-black);
+
+  img {
+    width: 2.8rem;
+    height: 2.8rem;
+    border-radius: 100%;
+  }
+
+  p {
+    padding: 0 0 0 0.6rem;
+    font-size: 1.4rem;
+    line-height: 1.6rem;
+    color: var(--color-black);
+
+    @media screen and (min-width:375px) and (max-width:768px) {
+      display: none;
+    }
+  }
+`
+
+const Login = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 12.8rem;
+  height: 5.4rem;
+  cursor: pointer;
+  background-image: linear-gradient(135deg, #6d6afe 0%, #6ae3fe 100%);
+  border-radius: 0.8rem;
+  color: var(--color-very-light-gray);
+  font-size: 2.8rem;
+  font-weight: 600;
+
+  @media screen and (min-width:375px) and (max-width:768px) {
+    width: 8rem;
+    height: 3.7rem;
+    font-size: 2.4rem;
+  }
+`
 
 function Nav({ 
   profileEmail,
   profileImg
 }){
   return (
-    <nav>
-      <h1 className="logo">
-        <a href="/">
-          <img src={logo} alt="홈으로 연결된 Linkbrary 로고" />
-        </a>
-      </h1>
-      <div className="profile">
+    <NavContainer>
+      <Logo Link to="/">
+        <img src={logo} alt="홈으로 연결된 Linkbrary 로고" />
+      </Logo>
+      <div>
         {profileEmail ? 
-        <a href="/">
+        <Email Link to="/">
           <img src={profileImg} alt="프로필 이미지" />
-          <p className="text mobile-hide">
-            {profileEmail}
-          </p>
-        </a> :
-        <a href="signin.html" className="cta">
+          <p>{profileEmail}</p>
+        </Email> :
+        <Login Link to="/">
           <span>로그인</span>
-        </a>}
+        </Login>}
       </div>
-    </nav>
+    </NavContainer>
   )
 }
 
