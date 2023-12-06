18aff4b406c687f3263c827225596bce592cdc77
dkile <dkile532@naver.com>
2023-12-06 14:25:50 +0900

    Refactor: data property 제거

--- src/components/folder/FolderTabMenuList.jsx
+++ src/components/folder/FolderTabMenuList.jsx
@@ -12,8 +12,9 @@ export default function FolderTabMenuList({
       <li>
         <button
           onClick={() => handleFolderTabMenuClick({ id: null, name: "전체" })}
-          data-current={currentFolder.id === null}
-          className="flex-shrink-0 rounded-[0.5rem] border-[0.1rem] border-u-primary px-[1rem] py-[0.6rem] text-[1.6rem] data-[current=true]:bg-u-primary data-[current=true]:text-u-white tablet:px-[1.2rem] tablet:py-[0.8rem]"
+          className={`flex-shrink-0 rounded-[0.5rem] border-[0.1rem] border-u-primary px-[1rem] py-[0.6rem] text-[1.6rem] ${
+            currentFolder.id === null ? "bg-u-primary text-u-white" : ""
+          } tablet:px-[1.2rem] tablet:py-[0.8rem]`}
         >
           전체
         </button>
@@ -27,8 +28,9 @@ export default function FolderTabMenuList({
                 name: folder.name,
               })
             }
-            data-current={currentFolder.id === folder.id}
-            className="flex-shrink-0 rounded-[0.5rem] border-[0.1rem] border-u-primary px-[1rem] py-[0.6rem] text-[1.6rem] data-[current=true]:bg-u-primary data-[current=true]:text-u-white tablet:px-[1.2rem] tablet:py-[0.8rem]"
+            className={`flex-shrink-0 rounded-[0.5rem] border-[0.1rem] border-u-primary px-[1rem] py-[0.6rem] text-[1.6rem] ${
+              currentFolder.id === folder.id ? "bg-u-primary text-u-white" : ""
+            } tablet:px-[1.2rem] tablet:py-[0.8rem]`}
           >
             {folder.name}
           </button>
