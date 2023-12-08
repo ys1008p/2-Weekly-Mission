a4fcb460bfcd3badec782b15af12cb726dc570da
김우현 <rladngus133@naver.com>
2023-12-08 13:37:50 +0900

    remove: handleSideBtn 함수 삭제(불필요)

--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -7,9 +7,6 @@ import { useState } from "react";
 
 function Folder({ psFolderData, handleData, folderName }) {
   const [sideBtnLender, setSideBtnLender] = useState(false);
-  function handleSideBtn(data) {
-    setSideBtnLender(data);
-  }
 
   return (
     <>
@@ -19,7 +16,7 @@ function Folder({ psFolderData, handleData, folderName }) {
         <FolderFilterButtonList
           psFolderData={psFolderData} // 폴더전체데이터
           handleData={handleData} // 누르는 버튼 데이터 보내기
-          handleSideBtn={handleSideBtn} // 각 폴더 누를때 사이드버튼 true false값 반환하는 함수
+          handleSideBtn={setSideBtnLender} // 각 폴더 누를때 사이드버튼 true false값 반환하는 함수
           sideBtnLender={sideBtnLender} // 사이드버튼 렌더값
           folderName={folderName} // 폴더이름표시
         />
