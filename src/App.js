68d1be80f24667ffc3eda2c83e07e5950e73d7f2
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2023-12-08 16:38:21 +0900

    feat: 회원가입 페이지 경로 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
 import SharedPage from './page/SharedPage';
 import FolderPage from './page/FolderPage';
+import SignupPage from './page/SignupPage';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Navigate replace to="/folder" />} />
+        <Route path="/" element={<Navigate replace to="/signup" />} />
+        <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/shared" element={<SharedPage />}></Route>
         <Route path="/folder" element={<FolderPage />}></Route>
       </Routes>
