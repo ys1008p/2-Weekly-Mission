d544a002fec4648fb028a2642bfc02a88e8bfd03
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2023-12-08 15:57:25 +0900

    design: JoinLink.js 추가

---
+++ src/components/JoinLink.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import logo from '../assets/logo.svg'
+
+const Logo = styled.div`
+  text-align: center;
+
+  img {
+    width: 210px;
+  }
+
+  span {
+    overflow: hidden;
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    clip-path: rect(0 0 0 0);
+  }
+`;
+
+const Link = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+  margin: 16px 0 0;
+
+  a {
+    color: var(--primary-color);
+    font-weight: 600;
+    line-height: 19px;
+    border-bottom: 1px solid var(--primary-color);
+  }
+`;
+
+function JoinLink() {
+  return (
+    <div>
+      <Logo>
+        <a href="/">
+          <img src={logo} alt="Linkbrary 로고" />
+          <span>Linkbrary</span>
+        </a>
+      </Logo>
+
+      <Link>
+        <p>이미 회원이신가요?</p>
+        <a href="/">로그인 하기</a>
+      </Link>
+    </div>
+  );
+}
+
+export default JoinLink;
