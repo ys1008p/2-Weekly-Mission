fdac73b8bd0e089fc383ff02ce9fa71647718880
Chyyni <catharina0409@gmail.com>
2023-12-31 15:49:33 +0900

    feat: 헤더의 추가하기을 누르면 링크를 폴더에 추가할 수 있는 모달 띄우기

--- src/components/AddLink.jsx
+++ src/components/AddLink.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import CTA from './CTA';
 import styles from './AddLink.module.css';
+import ModalLayout from './ModalLayout';
+import AddToFolderModal from './AddToFolderModal';
 
 export default function AddLink() {
   const [url, setUrl] = useState('');
+  const [addToFolderModalOpen, setAddToFolderModalOpen] = useState(false);
+
   return (
-    <form className={styles.form}>
+    <form
+      className={styles.form}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (!url) return;
+        setAddToFolderModalOpen(true);
+      }}
+    >
       <input
         className={styles.input}
         type='url'
@@ -16,6 +27,11 @@ export default function AddLink() {
         placeholder='🔗 링크를 추가해 보세요.'
       />
       <CTA text='추가하기' className='fixedWidth' />
+      {addToFolderModalOpen && (
+        <ModalLayout setModalOpen={setAddToFolderModalOpen} modalTitle='폴더에 추가' details={url}>
+          <AddToFolderModal url={url} setAddToFolderModalOpen={setAddToFolderModalOpen} />
+        </ModalLayout>
+      )}
     </form>
   );
 }
