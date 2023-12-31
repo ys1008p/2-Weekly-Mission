22ccd3c6e2059d6a81232db7fa5d6e6e576c3390
Chyyni <catharina0409@gmail.com>
2023-12-31 15:52:23 +0900

    feat: 새로운 폴더를 추가할 수 있는 모달 구현

--- src/components/FolderButtons.jsx
+++ src/components/FolderButtons.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useGetData from '../hooks/useGetData';
+import ModalLayout from './ModalLayout';
+import CTA from './CTA';
 import { ReactComponent as AddIcon } from '../images/add.svg';
 import styles from './FolderButtons.module.css';
 
 export default function FolderButtons({ folderId, handleFolderClick }) {
   const [loading, error, user] = useGetData('users/1/folders');
+  const [addFolderModalOpen, setAddFolderModalOpen] = useState(false);
+  const [folderName, setFolderName] = useState('');
 
   if (loading) return <div>loading..</div>;
   if (error) return <p>{error}</p>;
@@ -14,9 +18,7 @@ export default function FolderButtons({ folderId, handleFolderClick }) {
   const list = folderList.map(({ id, name }) => (
     <li key={id}>
       <button
-        className={`${styles.button} ${
-          folderId === id ? styles.activeButton : ''
-        }`}
+        className={`${styles.button} ${folderId === id ? styles.activeButton : ''}`}
         onClick={() => {
           handleFolderClick(id, name);
         }}
@@ -26,14 +28,26 @@ export default function FolderButtons({ folderId, handleFolderClick }) {
     </li>
   ));
 
+  const handleAddFolder = async (e) => {
+    e.preventDefault();
+    if (!folderName) return;
+    const addFolder = async () => {
+      await fetch(`https://bootcamp-api.codeit.kr/api/folders`, {
+        method: 'POST',
+        body: { name: folderName },
+      });
+    };
+    await addFolder();
+    setFolderName('');
+    setAddFolderModalOpen(false);
+  };
+
   return (
     <div className={styles.container}>
       <ul className={styles.buttonList}>
         <li>
           <button
-            className={`${styles.button} ${
-              !folderId ? styles.activeButton : ''
-            }`}
+            className={`${styles.button} ${!folderId ? styles.activeButton : ''}`}
             onClick={() => {
               handleFolderClick(null, '전체');
             }}
@@ -43,14 +57,23 @@ export default function FolderButtons({ folderId, handleFolderClick }) {
         </li>
         {list}
       </ul>
-      <button>
-        <AddIcon
-          className={styles.addButton}
-          src={AddIcon}
-          alt='폴더 추가하기 버튼'
-          fill='#6d6afe'
-        />
+      <button onClick={() => setAddFolderModalOpen(true)}>
+        <AddIcon className={styles.addButton} src={AddIcon} alt='폴더 추가하기 버튼' fill='#6d6afe' />
       </button>
+      {addFolderModalOpen && (
+        <ModalLayout setModalOpen={setAddFolderModalOpen} modalTitle='폴더 추가'>
+          <form className={styles.modalForm} onSubmit={handleAddFolder}>
+            <input
+              className={styles.modalInput}
+              value={folderName}
+              type='text'
+              placeholder='내용 입력'
+              onChange={(e) => setFolderName(e.target.value)}
+            />
+            <CTA text='추가하기' className='variableWidth' />
+          </form>
+        </ModalLayout>
+      )}
     </div>
   );
 }
