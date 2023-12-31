5127ec2c70225f3bb6ff1c7c5916d21cf9ef7afc
Chyyni <catharina0409@gmail.com>
2023-12-31 15:50:35 +0900

    feat: 개별 링크의 케밥 버튼에 드롭박스 기능 구현

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,11 @@ import emptyImage from '../images/emptyImage.svg';
 import StarImg from '../images/star.svg';
 import KebabImg from '../images/kebab.svg';
 import './Card.css';
+import Dropdown from './Dropdown';
 
-export default function Card({
-  link: { title, created_at, description, image_source, url },
-}) {
+export default function Card({ link: { id, title, created_at, description, image_source, url } }) {
   const [logTime, setLogTime] = useState('');
+  const [viewDropBox, setViewDropBox] = useState(false);
 
   const date = getDate(created_at);
 
@@ -21,26 +21,32 @@ export default function Card({
         {image_source ? (
           <img className='card-image' src={image_source} alt={title} />
         ) : (
-          <img
-            className='card-image'
-            src={emptyImage}
-            alt={`비어있는 ${title} 이미지`}
-          />
+          <img className='card-image' src={emptyImage} alt={`비어있는 ${title} 이미지`} />
         )}
       </div>
       <button className='add-bookmark-icon'>
         <img src={StarImg} alt='즐겨찾기 추가' />
       </button>
       <div className='card-info'>
-        <p className='card-log-time'>{logTime}</p>
+        <div className='card-time-and-kebab'>
+          <p className='card-log-time'>{logTime}</p>
+          <div className='kebab-container'>
+            <button
+              className='kebab-icon'
+              onClick={(e) => {
+                e.preventDefault();
+                setViewDropBox(!viewDropBox);
+              }}
+            >
+              <img src={KebabImg} alt='개별 링크 삭제 혹은 폴더에 추가하기 기능 리스트 토글' />
+            </button>
+            {viewDropBox && (
+              <Dropdown className='dropdown-position' setViewDropBox={setViewDropBox} url={url} id={id} />
+            )}
+          </div>
+        </div>
         <p className='card-description'>{description}</p>
         <p className='card-date'>{date}</p>
-        <button className='kebab-icon'>
-          <img
-            src={KebabImg}
-            alt='개별 링크 삭제 혹은 폴더에 추가하기 기능 리스트 토글'
-          />
-        </button>
       </div>
     </a>
   );
