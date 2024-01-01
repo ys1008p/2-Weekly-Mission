db85b9472065d7e891a40b0a80a80cfd80e4d1df
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-01-01 19:35:44 +0900

    feat: 추가 검색할 내용 보여주기

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import search from '../assets/ico-search.png';
 import searchRemove from '../assets/ico-search-remove.svg';
+import { useState } from 'react';
 
 const SearchBarContainer = styled.div`
   position: relative;
@@ -11,8 +12,7 @@ const SearchBarContainer = styled.div`
     display: block;
     position: absolute;
     left: 1.7rem;
-    top: 50%;
-    transform: translateY(-50%);
+    top: 1.6rem;
     width: 1.6rem;
     height: 1.6rem;
     background: url('${search}') no-repeat;
@@ -44,10 +44,37 @@ const Input = styled.input`
   }
 `;
 
+const Text = styled.p`
+  display: ${({ value }) => (value ? 'block' : 'none')};
+  margin: 4rem 0 0;
+  font-size: 3.2rem;
+  font-weight: 600;
+  color: var(--gray60);
+  line-height: 3.8rem;
+
+  span {
+    color: var(--gray100);
+    font-size: 3.2rem;
+  }
+`;
+
 function SearchBar() {
+  const [value, setValue] = useState('');
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) setValue(e.target.value);
+  };
+
   return (
     <SearchBarContainer>
-      <Input type="search" placeholder="링크를 검색해 보세요." />
+      <Input
+        type="search"
+        placeholder="링크를 검색해 보세요."
+        onKeyDown={handleKeyDown}
+      />
+      <Text value={value}>
+        <span>{value}</span>으로 검색한 결과입니다.
+      </Text>
     </SearchBarContainer>
   );
 }
